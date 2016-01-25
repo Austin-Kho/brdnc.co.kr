89b30db34b4d1f1a44e6ea121b33d9c7e420eb5f
cigiko <cigiko@naver.com>
2016-01-25 13:57:11 +0900

    동호수 상황표 DB변경 표시 수정 - 청·계약 관련 작업 진행 중

--- cms/_menu1/sales_rp3.php
+++ cms/_menu1/sales_rp3.php
@@ -102,19 +102,33 @@
 						</form>
 						<div style="margin-top:10px; height:25px;">
 							<div style="float:left; height:22px; width:80px;"></div>
+
+							<!-- 이부분 쿼리 향후 다시 작업 할 것 -->
 							<?
+								// 공공급세대 및 유보세대 구하기
 								$data_qry = "SELECT COUNT(*) AS total,
-														 SUM(is_except) AS except,
-														 SUM(is_pro_cont) AS pro_cont,
-														 SUM(is_contract) AS contract
-											   FROM cms_project_data WHERE pj_seq='$pj_list' ";
+														 SUM(is_hold) AS hold
+											   FROM cms_project2_indi_table WHERE pj_seq='$pj_list' ";
 								$data_rlt = mysql_query($data_qry, $connect);
 								$data_row = mysql_fetch_array($data_rlt);
+
+								// 청약세대 구하기
+								$data_qry1 = "SELECT SUM(is_pro_cont) AS pro_cont,
+														 				 SUM(is_contract) AS contract
+											   FROM cms_project_data WHERE pj_seq='$pj_list' ";
+								$data_rlt1 = mysql_query($data_qry1, $connect);
+								$data_row1 = mysql_fetch_array($data_rlt1);
+
+
+
+
 							?>
 							<div style="float:left; height:22px; width:80px; background-color:#eaedf4; padding-top:2px; text-align:center; border-width:1px 0 0 1px; border-style:solid; border-color:#cdcdcd; color:#000000;">총 세대수</div>
 							<div style="float:left; height:22px; width:75px; padding:2px 10px 0 0; text-align:right; border-width:1px 0 0 1px; border-style:solid; border-color:#cdcdcd;"><?if($pj_list) echo number_format($data_row[total])." 세대";?></div>
-							<div style="float:left; height:22px; width:80px; background-color:#9e9e9e; color:#cccccc; padding-top:2px; text-align:center; border-width:1px 0 0 1px; border-style:solid; border-color:#cdcdcd;">기분양 세대</div>
-							<div style="float:left; height:22px; width:75px; padding:2px 10px 0 0; text-align:right; border-width:1px 1px 0 1px; border-style:solid; border-color:#cdcdcd; margin-right:30px;"><?if($pj_list) echo number_format($data_row[except])." 세대";?></div>
+							<div style="float:left; height:22px; width:80px; background-color:#9e9e9e; color:#DFDFDF; padding-top:2px; text-align:center; border-width:1px 0 0 1px; border-style:solid; border-color:#cdcdcd;">유보 세대</div>
+							<div style="float:left; height:22px; width:75px; padding:2px 10px 0 0; text-align:right; border-width:1px 1px 0 1px; border-style:solid; border-color:#cdcdcd; margin-right:30px;"><?if($pj_list) echo number_format($data_row[hold])." 세대";?></div>
+
+							<!-- 타입컬러 범례 1열 표시 시작 -->
 							<?
 								$color_rlt = mysql_query("SELECT type_name, type_color FROM cms_project1_info WHERE seq='$pj_list' ", $connect); // 타입별 컬러 구하기
 								$color_row = mysql_fetch_array($color_rlt);
@@ -128,12 +142,16 @@
 							<div style="float:left; height:12px; width:50px; padding-left:5px; font-size:7pt;"><?=$type[$i]?></div>
 							<? } ?>
 						</div>
+						<!-- 타입컬러 범례 1열 표시 종료 -->
+
 						<div style="clear:left; height:25px;">
 							<div style="float:left; height:22px; width:80px;"></div>
-							<div style="float:left; height:22px; width:80px; background-color:#e7e4d3; padding-top:2px; text-align:center; border-width:1px 0 0 1px; border-style:solid; border-color:#cdcdcd; color:#000000;">청약 세대</div>
+							<div style="float:left; height:22px; width:80px; background-color:#C6CEFF; padding-top:2px; text-align:center; border-width:1px 0 0 1px; border-style:solid; border-color:#cdcdcd; color:#000000;">청약 세대</div>
 							<div style="float:left; height:22px; width:75px; padding:2px 10px 0 0; text-align:right; color:#009900;  border-width:1px 0 0 1px; border-style:solid; border-color:#cdcdcd;"><?if($pj_list) echo number_format($data_row[pro_cont])." 세대";?></div>
-							<div style="float:left; height:22px; width:80px; background-color:#ebc694; color:#000000; padding-top:2px; text-align:center; border-width:1px 0 0 1px; border-style:solid; border-color:#cdcdcd;">계약 세대</div>
+							<div style="float:left; height:22px; width:80px; background-color:#E0FD6C; color:#000000; padding-top:2px; text-align:center; border-width:1px 0 0 1px; border-style:solid; border-color:#cdcdcd;">계약 세대</div>
 							<div style="float:left; height:22px; width:75px; padding:2px 10px 0 0; text-align:right; color:#0066ff; border-width:1px 1px 0 1px; border-style:solid; border-color:#cdcdcd; margin-right:30px;"><?if($pj_list) echo number_format($data_row[contract])." 세대";?></div>
+
+							<!-- 타입컬러 범례 2열 표시 시작 -->
 							<?
 								if(count($type)>6&&count($type)<=12) $roof_num = count($type); else if(count($type)>12) $roof_num = 6;
 								for($i=6; $i<$roof_num; $i++){
@@ -143,12 +161,16 @@
 							<div style="float:left; height:12px; width:50px; padding-left:5px; font-size:7pt;"><?=$type[$i]?></div>
 							<? } ?>
 						</div>
+						<!-- 타입컬러 범례 2열 표시 종료 -->
+
 						<div style="clear:left; height:25px;">
 							<div style="float:left; height:22px; width:80px;"></div>
 							<div style="float:left; height:22px; width:80px; background-color:#d5e2ec; color:#000000; padding-top:2px; text-align:center; border-width:1px 0 1px 1px; border-style:solid; border-color:#cdcdcd;">합 계</div>
 							<div style="float:left; height:22px; width:75px; padding:2px 10px 0 0; text-align:right; color:#0000cc; border-width:1px 0 1px 1px; border-style:solid; border-color:#cdcdcd;"><?if($pj_list) echo number_format($data_row[pro_cont]+$data_row[contract])." 세대";?></div>
 							<div style="float:left; height:22px; width:80px; background-color:#edffb5; color:#000000; padding-top:2px; text-align:center; border-width:1px 0 1px 1px; border-style:solid; border-color:#cdcdcd;">잔여 세대수</div>
-							<div style="float:left; height:22px; width:75px; padding:2px 10px 0 0; text-align:right; color:#00cc00; border-width:1px 1px 1px 1px; border-style:solid; border-color:#cdcdcd; margin-right:30px;"><?if($pj_list) echo number_format($data_row[total]-($data_row[except]+$data_row[pro_cont]+$data_row[contract]))." 세대";?></div>
+							<div style="float:left; height:22px; width:75px; padding:2px 10px 0 0; text-align:right; color:#00cc00; border-width:1px 1px 1px 1px; border-style:solid; border-color:#cdcdcd; margin-right:30px;"><?if($pj_list) echo number_format($data_row[total]-($data_row[hold]+$data_row[pro_cont]+$data_row[contract]))." 세대";?></div>
+
+							<!-- 타입컬러 범례 3열 표시 시작 -->
 							<?
 								if(count($type)>13) $roof_num = count($type);
 								for($i=13; $i<$roof_num; $i++){
@@ -157,6 +179,8 @@
 							<div style="float:left; height:12px; width:18px; background-color:<?=$color[$i]?>;"></div>
 							<div style="float:left; height:12px; width:50px; padding-left:5px; font-size:7pt;"><?=$type[$i]?></div>
 							<? } ?>
+							<!-- 타입컬러 범례 3열 표시 종료 -->
+
 						</div>
 						<div style="height:12px; border-width:0 0 1px 0; border-style:solid; border-color:#dddddd; margin-bottom:10px;"></div>
 						<?
@@ -165,12 +189,13 @@
 						<div style="height:80px; text-align:center; padding-top:80px;">조회 할 프로젝트를 선택하여 주십시요!</div>
 						<?
 							}
-							$max_rlt = mysql_query("SELECT MAX(pj_ho) AS max_ho FROM cms_project_data WHERE pj_seq='$pj_list' ", $connect); // 해당 단지 최 고층 구하기
+							//
+							$max_rlt = mysql_query("SELECT MAX(ho) AS max_ho FROM cms_project2_indi_table WHERE pj_seq='$pj_list' ", $connect); // 해당 단지 최 고층 구하기
 							$max_row = mysql_fetch_array($max_rlt);
 							if(strlen($max_row[max_ho])==3) $max_floor = substr($max_row[max_ho], -3,1);
 							if(strlen($max_row[max_ho])==4) $max_floor = substr($max_row[max_ho], -4,2);
 
-							$dong_qry = " SELECT pj_dong FROM cms_project_data WHERE pj_seq='$pj_list' GROUP BY pj_dong ";  // 해당 단지 동 수 및 리스트 구하기
+							$dong_qry = " SELECT dong FROM cms_project2_indi_table WHERE pj_seq='$pj_list' GROUP BY dong ";  // 해당 단지 동 수 및 리스트 구하기
 							$dong_rlt = mysql_query($dong_qry, $connect);
 							$num_rows = mysql_num_rows($dong_rlt);
 							while($dong_rows = mysql_fetch_array($dong_rlt)){ // 해당 동 만큼 반복
@@ -181,10 +206,10 @@
 							<td>
 						<?
 								for($j=0; $j<$max_floor; $j++){ // 최고층 수만큼 반복
-									$ho_qry = "SELECT MIN(RIGHT(pj_ho,2)) AS st_line, MAX(RIGHT(pj_ho,2)) AS dong_line
-												 FROM cms_project_data WHERE pj_seq='$pj_list' AND pj_dong='$dong_rows[pj_dong]' "; // 각 동별 라인수 구하기
+									$ho_qry = "SELECT MIN(RIGHT(ho,2)) AS st_line, MAX(RIGHT(ho,2)) AS dong_line
+												 FROM cms_project2_indi_table WHERE pj_seq='$pj_list' AND dong='$dong_rows[dong]' "; // 각 동별 라인수 구하기
 									$ho_rlt = mysql_query($ho_qry, $connect);
-									$ho_rows = mysql_fetch_array($ho_rlt);  // 강 동별 라인 만큼 반복
+									$ho_rows = mysql_fetch_array($ho_rlt);  // 반복하기 위한 각 동별 라인 수
 									$floor_num = $max_floor-$j; // 층수
 
 									for($i=0; $i<$ho_rows[dong_line]; $i++){ // 라인수 만큼 반복
@@ -195,17 +220,22 @@
 											$clear_css = "";
 											$bor_wid = "border-width:1px 1px 0 0;";
 										}
-										$line_num = str_pad($i+1, 2, 0, STR_PAD_LEFT); // 라인
-										$ho_num = $floor_num.$line_num; // 호수
-										$ho_ck_qry = "SELECT pj_ho, type_ho, is_except, is_pro_cont, is_contract, pro_contractor, pro_cont_date, contractor, cont_date
-														 FROM cms_project_data WHERE pj_seq='$pj_list' AND pj_dong='$dong_rows[pj_dong]' AND pj_ho='$ho_num' ";
+										$line_num = str_pad($i+1, 2, 0, STR_PAD_LEFT); // 라인 텍스트
+										$ho_num = $floor_num.$line_num; // 호수 텍스트
+
+
+										// 타입, 호수, 유보여부, 청약여부, 계약여부, 청약자, 청약일, 계약자, 계약일 구하기 ???????
+										// $ho_ck_qry = "SELECT pj_ho, type_ho, is_except, is_pro_cont, is_contract, pro_contractor, pro_cont_date, contractor, cont_date
+										//				 FROM cms_project_data WHERE pj_seq='$pj_list' AND pj_dong='$dong_rows[pj_dong]' AND pj_ho='$ho_num' ";
+										$ho_ck_qry = "SELECT type, ho, is_hold
+														 FROM cms_project2_indi_table WHERE pj_seq='$pj_list' AND dong='$dong_rows[dong]' AND ho='$ho_num' ";
 										$ho_ck_rlt = mysql_query($ho_ck_qry, $connect);
 										$ho_ck_row = mysql_fetch_array($ho_ck_rlt);
 
 										/////////////////////////////////////// 동호수 표시칸 css S //////////////////////////////////////////
 										if($ho_ck_row) {   // div 번호에 해당 호수가 있으면,
 											$bor_col = "border-color:#cccccc;";
-											$div_col = "background-color:".$type_color[$ho_ck_row[type_ho]].";";
+											$div_col = "background-color:".$type_color[$ho_ck_row[type]].";";
 										}else {
 											if($ho_num<400){ // 호수가 없는 4층 이하(피로티)인 경우
 												$bor_col = "border-color:#c3c3c3;";
@@ -217,7 +247,10 @@
 										}
 										/////////////////////////////////////// 동호수 표시칸 css E //////////////////////////////////////////
 
-										///////////////////////////////////// 청약/ 계약 상태 표시란 css S ///////////////////////////////////////////////
+
+
+										////---------->> 이 란 추후 수정 할 것
+										///////////////////////////////////// 청약 / 계약 상태 표시란 css S ////////////////////////////////////
 										if($ho_ck_row[is_pro_cont]==1){ // 청약인 경우
 											$condi = "<a href='#;' style='color:#FFFFFF; font-size:8pt;' title='청약자 : ".$ho_ck_row[pro_contractor]." (".$ho_ck_row[pro_cont_date].")'>청약</a>";
 											$con_css = "background-color:#ff6600;";
@@ -240,7 +273,7 @@
 						?>
 							<div style="<?=$clear_css?> float:left;">
 								<div style="font-size:8pt; width:30px; height:14px; text-align:center; border-style:solid; <?=$bor_wid." ".$bor_col." ".$div_col?>">
-									<a href="#;" title="<?=$ho_ck_row[type_ho]." TYPE"?>" style="font-size:8pt; color:#000000;"><?=$ho_ck_row[pj_ho]?></a>
+									<a href="#;" title="<?=$ho_ck_row[type_ho]." TYPE"?>" style="font-size:8pt; color:#000000;"><?=$ho_ck_row[ho]?></a>
 								</div>
 								<div style="font-size:7pt; width:30px; height:14px; text-align:center;border-style:solid; <?=$bor_wid." ".$bor_col." ".$con_css?>"><?=$condi?></div>
 							</div>
@@ -251,7 +284,7 @@
 							</td>
 						</tr>
 						</table>
-							<div style="height:25px; padding-top:5px; text-align:center; background-color:#4a7da4; margin-bottom:10px; color:white; border:1px; border-style:solid; border-color:#3e3e3e;"><?=$dong_rows[pj_dong]."동"?></div>
+							<div style="height:25px; padding-top:5px; text-align:center; background-color:#4a7da4; margin-bottom:10px; color:white; border:1px; border-style:solid; border-color:#3e3e3e;"><?=$dong_rows[dong]."동"?></div>
 						</div>
 						<div style="float:left; width:10px; height:20px; padding-top:20px;"></div>
 						<?
