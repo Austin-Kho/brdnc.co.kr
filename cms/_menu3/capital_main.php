0bcc75fec5130743dd49a28f757f7778afbd05d5
cigiko <cigiko@naver.com>
2016-01-27 17:49:13 +0900

    html 코드 정렬 변경

--- cms/_menu3/capital_main.php
+++ cms/_menu3/capital_main.php
@@ -15,71 +15,71 @@
 ?>
 <!DOCTYPE HTML>
 <html>
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
-  <title><?=$doc_title?></title>
-  <link rel="shortcut icon" href="<?=$cms_url?>images/cms.ico">
-  <link type="text/css" rel="stylesheet" href="<?=$cms_url?>common/cms.css">
-  <script src="../common/global.js"></script>
-  <script src="../common/_menu3.js"></script>
-  <script src="../include/calendar/calendar.js"></script>
-  <script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
-  <script type="text/javascript">
-   $(document).ready(function(){
-    $("#loading").css("display","none");
-   });
-  </script>
-  <script type="text/javascript">
-	<!--
-		function term_put(a,b,term){
-			if(term=='d')var term="<?=date('Y-m-d')?>";
-			if(term=='w')var term="<?=date('Y-m-d',strtotime ('-1 weeks'))?>";
-			if(term=='m')var term="<?=date('Y-m-d',strtotime ('-1 months'))?>";
-			if(term=='3m')var term="<?=date('Y-m-d',strtotime ('-3 months'))?>";
-			document.getElementById(a).value = term;
-			document.getElementById(b).value = "<?=date('Y-m-d')?>";
-		}
-		function _del(code){
-			if(aa=confirm('정말 삭제하시겠습니까?')){
-				location.href='capital_del.php?del_code='+code
-			}else{
-				return false;
+	<head>
+	  <meta charset="UTF-8">
+	  <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
+	  <title><?=$doc_title?></title>
+	  <link rel="shortcut icon" href="<?=$cms_url?>images/cms.ico">
+	  <link type="text/css" rel="stylesheet" href="<?=$cms_url?>common/cms.css">
+	  <script src="../common/global.js"></script>
+	  <script src="../common/_menu3.js"></script>
+	  <script src="../include/calendar/calendar.js"></script>
+	  <script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
+	  <script type="text/javascript">
+	   $(document).ready(function(){
+	    $("#loading").css("display","none");
+	   });
+	  </script>
+	  <script type="text/javascript">
+		<!--
+			function term_put(a,b,term){
+				if(term=='d')var term="<?=date('Y-m-d')?>";
+				if(term=='w')var term="<?=date('Y-m-d',strtotime ('-1 weeks'))?>";
+				if(term=='m')var term="<?=date('Y-m-d',strtotime ('-1 months'))?>";
+				if(term=='3m')var term="<?=date('Y-m-d',strtotime ('-3 months'))?>";
+				document.getElementById(a).value = term;
+				document.getElementById(b).value = "<?=date('Y-m-d')?>";
 			}
-		}
-    //-->
-  </script>
-</head>
+			function _del(code){
+				if(aa=confirm('정말 삭제하시겠습니까?')){
+					location.href='capital_del.php?del_code='+code
+				}else{
+					return false;
+				}
+			}
+	    //-->
+	  </script>
+	</head>
 
-<body onclick="cal_del();">
-<div id="loading" style="padding-top:530px;"><img src="../images/loading.gif"><br>loading...</div>
-<div id="wrap">
-	<div id="header">
-		<? include '../include/t_menu.php'; ?>
-	</div>
-	<div id="content">
-		<?
-			if(!$_SESSION['p_id']){
-		?>
-		<div style="width:1080px; height:650px; text-align:center; display: table-cell; vertical-align: middle;">
-			<p>로그인 정보가 없습니다. 다시 로그인하여 주십시요!</p>
-			<input type="button" value="로그인" class="sub_bt1" onclick="location.href='<?=$cms_url?>member/login_form.php';">
-			<input type="button" value=" 닫 기 " class="sub_bt1" onclick="window.self.close()">
+	<body onclick="cal_del();">
+		<div id="loading" style="padding-top:530px;"><img src="../images/loading.gif"><br>loading...</div>
+		<div id="wrap">
+			<div id="header">
+				<? include '../include/t_menu.php'; ?>
+			</div>
+			<div id="content">
+				<?
+					if(!$_SESSION['p_id']){
+				?>
+				<div style="width:1080px; height:650px; text-align:center; display: table-cell; vertical-align: middle;">
+					<p>로그인 정보가 없습니다. 다시 로그인하여 주십시요!</p>
+					<input type="button" value="로그인" class="sub_bt1" onclick="location.href='<?=$cms_url?>member/login_form.php';">
+					<input type="button" value=" 닫 기 " class="sub_bt1" onclick="window.self.close()">
+				</div>
+				<? }else{?>
+				<div>
+					<!-- ===================================== Head end / body start ===================================== -->
+					<?
+						if(!$m_di||$m_di==1) include "capital.php";
+						if($m_di==2) include "labor.php";
+					?>
+					<!-- ===================================== body end / Foot start ===================================== -->
+				</div>
+				<? } ?>
+			</div>
 		</div>
-		<? }else{?>
-		<div>
-			<!-- ===================================== Head end / body start ===================================== -->
-			<?
-				if(!$m_di||$m_di==1) include "capital.php";
-				if($m_di==2) include "labor.php";
-			?>
-			<!-- ===================================== body end / Foot start ===================================== -->
+		<div id="footwrap">
+			<div id="footer"><?=$doc_copyright?></div>
 		</div>
-		<? } ?>
-	</div>
- </div>
- <div id="footwrap">
-	<div id="footer"><?=$doc_copyright?></div>
-</div>
-</body>
+	</body>
 </html>
--- cms/_menu4/project_main.php
+++ cms/_menu4/project_main.php
@@ -15,53 +15,52 @@
 ?>
 <!DOCTYPE HTML>
 <html>
- <head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
-    <title><?=$doc_title?></title>
-	<link rel="shortcut icon" href="<?=$cms_url?>images/cms.ico">
-	<link type="text/css" rel="stylesheet" href="<?=$cms_url?>common/cms.css">
-	<script src="../common/global.js"></script>
-	<script src="../common/_menu4.js"></script>
-	<script src="../include/calendar/calendar.js"></script>
-	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
-    <script type="text/javascript">
-	   $(document).ready(function(){
-	    $("#loading").css("display","none");
-	   });
-    </script>
- </head>
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
+	    <title><?=$doc_title?></title>
+		<link rel="shortcut icon" href="<?=$cms_url?>images/cms.ico">
+		<link type="text/css" rel="stylesheet" href="<?=$cms_url?>common/cms.css">
+		<script src="../common/global.js"></script>
+		<script src="../common/_menu4.js"></script>
+		<script src="../include/calendar/calendar.js"></script>
+		<script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
+	    <script type="text/javascript">
+		   $(document).ready(function(){
+		    $("#loading").css("display","none");
+		   });
+	    </script>
+	</head>
 
- <body onclick="cal_del();">
- <div id="loading" style="padding-top:530px;"><img src="../images/loading.gif"><br>loading...</div>
- <!-- ============================ 3 ============================ -->
- <div id="wrap">
-	<div id="header">
-	<? include '../include/t_menu.php'; ?>
-	</div>
-	<div id="content">
-		<?
-			if(!$_SESSION['p_id']){
-		?>
-		<div style="width:1080px; height:650px; text-align:center; display: table-cell; vertical-align: middle;">
-			<p>로그인 정보가 없습니다. 다시 로그인하여 주십시요!</p>
-			<input type="button" value="로그인" class="sub_bt1" onclick="location.href='<?=$cms_url?>member/login_form.php';">
-			<input type="button" value=" 닫 기 " class="sub_bt1" onclick="window.self.close()">
+	<body onclick="cal_del();">
+		<div id="loading" style="padding-top:530px;"><img src="../images/loading.gif"><br>loading...</div>
+		<div id="wrap">
+			<div id="header">
+				<? include '../include/t_menu.php'; ?>
+			</div>
+			<div id="content">
+				<?
+					if(!$_SESSION['p_id']){
+				?>
+				<div style="width:1080px; height:650px; text-align:center; display: table-cell; vertical-align: middle;">
+					<p>로그인 정보가 없습니다. 다시 로그인하여 주십시요!</p>
+					<input type="button" value="로그인" class="sub_bt1" onclick="location.href='<?=$cms_url?>member/login_form.php';">
+					<input type="button" value=" 닫 기 " class="sub_bt1" onclick="window.self.close()">
+				</div>
+				<? }else{?>
+				<div>
+					<!-- ============================ Head end / body start ============================ -->
+					<?
+						if(!$m_di||$m_di==1) include "progress.php";
+						if($m_di==2) include "contract.php";
+					?>
+					<!-- ============================ body end / Foot start ============================ -->
+				</div>
+				<? } ?>
+			</div>
 		</div>
-		<? }else{?>
-		<div>
-			<!-- ============================ Head end / body start ============================ -->
-			<?
-				if(!$m_di||$m_di==1) include "progress.php";
-				if($m_di==2) include "contract.php";
-			?>
-			<!-- ============================ body end / Foot start ============================ -->
+		<div id="footwrap">
+			<div id="footer"><?=$doc_copyright?></div>
 		</div>
-		<? } ?>
-	</div>
- </div>
- <div id="footwrap">
-	<div id="footer"><?=$doc_copyright?></div>
- </div>
- </body>
+	</body>
 </html>
