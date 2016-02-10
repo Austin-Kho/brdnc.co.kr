14bb14f77a55507e738c1bf191eb00cd0e94fb5c
cigiko <cigiko@naver.com>
2016-02-11 02:53:13 +0900

    유저가이드 삭제

--- application/controllers/Imsi_cms.php
+++ application/controllers/Imsi_cms.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Cms extends CI_Controller {
+class Imsi_cms extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
