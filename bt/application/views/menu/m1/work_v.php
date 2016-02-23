971f5a9514c991f82567fb02ab79f3348de0f62a
cigiko <cigiko@naver.com>
2016-02-24 02:16:54 +0900

    진짜 자자

--- bt/application/views/menu/m1/work_v.php
+++ bt/application/views/menu/m1/work_v.php
@@ -1,6 +1,6 @@
       <div style="height: 10px;"></div>
 
-      <ul class="nav nav-tabs" role="tablist" id="md_sub">
+      <ul class="nav nav-tabs" role="tablist">
         <li role="presentation" class="<?if( !$this->uri->segment(3) or $this->uri->segment(3)=='1') echo 'active'; else echo '';?>">
         	<a href="<?php echo $this->config->base_url(); ?>m1/work/1/"><strong>계 약 현 황</strong></a>
         </li>
