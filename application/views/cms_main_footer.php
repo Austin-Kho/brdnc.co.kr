527a882c85bd1990e296c2412be4bf91f08f3d77
cigiko <cigiko@naver.com>
2016-02-29 10:49:57 +0900

    메인푸터 경로 오류 수정

--- application/views/cms_main_footer.php
+++ application/views/cms_main_footer.php
@@ -1,8 +1,8 @@
 			<footer class="footer">
 				<div class="container" style="padding-top: 10px;">
-					<div style="float: left; margin: 0 15px 5px 0;"><img src="/bt/static/img/cms_b_logo.png" width="130px"></div>
+					<div style="float: left; margin: 0 15px 5px 0;"><img src="/static/img/cms_box_logo.png" width="120px"></div>
 					<div style="float: left;"><span class="text-muted"><!-- <strong>[주] 바램디앤씨</strong> --> <small>| 인천광역시 연수구 인천타워대로323 B-1506(송도동, 송도센트로드오피스) | 전화 : 032-858-9556 | 문의하기 : <a href='mailto:cigiko@naver.com' class='under'>cigiko@naver.com</a><br>
-					Copyright 2015-<?=date('Y')?> by BARAEM D&C Co.,LTD All rights reserved.</small></span></div>
+					Copyright 2015-<?php echo date('Y')?> by BARAEM D&C Co.,LTD All rights reserved.</small></span></div>
 				</div>
 			</footer>
 		</div> <!-- /container -->
@@ -11,8 +11,8 @@
 		================================================== -->
 		<!-- Placed at the end of the document so the pages load faster -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-		<script src="<?php echo $this->config->base_url(); ?>static/lib/bootstrap/js//bootstrap.min.js"></script>
-		<script src="<?php echo $this->config->base_url(); ?>static/js/docs.min.js"></script>
+		<script src="/static/lib/bootstrap/js//bootstrap.min.js"></script>
+		<script src="/static/js/docs.min.js"></script>
 		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
 		<!-- <script src="<?php echo $this->config->base_url(); ?>static/js/ie10-viewport-bug-workaround.js"></script> -->
 	</body>
