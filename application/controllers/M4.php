f5f9505005ba1e87d0cf275bfacbba72bca26d36
cigiko <cigiko@naver.com>
2016-04-04 23:47:57 +0900

    11

--- application/controllers/M4.php
+++ application/controllers/M4.php
@@ -12,6 +12,8 @@ class M4 extends CI_Controller {
 			redirect(base_url().'member/');
 		}
 		$this->load->model('main_m'); //모델 파일 로드
+		$this->load->model('m4_m'); //모델 파일 로드
+		$this->load->helper('alert'); // 경고창 헤퍼 로딩
 	}
 
 	/**
@@ -36,16 +38,16 @@ class M4 extends CI_Controller {
 	public function capital($mdi='', $sdi=''){
 		//$this->output->enable_profiler(TRUE); //프로파일러 보기//
 
-		if( !$this->uri->segment(3)) $mdi = 1; else $mdi = $this->uri->segment(3);
-		if( !$this->uri->segment(4)) $sdi = 1; else $sdi = $this->uri->segment(4);
+		$mdi = $this->uri->segment(3, 1);
+		$sdi = $this->uri->segment(4, 1);
 
 		$menu['s_di'] = array(
 			array('자금 일보', '입출금 내역', '입출금 등록'), // 첫번째 하위 메뉴
 			array('분 개 장', '일·월계표', '제무 제표'),                          // 두번째 하위 메뉴
 			array('자금 일보', '입출금 내역', '입출금 등록'), // 첫번째 하위 제목
-			array('분 개 장', '일·월계표', '주요 제무제표')                                  // 두번째 하위 제목
+			array('분 개 장', '일·월계표', '주요 제무제표')                    // 두번째 하위 제목
 		);
-
+		// 메뉴데이터 삽입 하여 메인 페이지 호출
 		$this->load->view('menu/m4/capital_v', $menu);
 
 		// 자금 현황 1. 자금일보 ////////////////////////////////////////////////////////////////////
@@ -53,9 +55,12 @@ class M4 extends CI_Controller {
 			// 조회 등록 권한 체크
 			$auth = $this->main_m->auth_chk('_m4_1_1', $this->session->userdata['user_id']);
 
-			if( !$auth['_m4_1_1'] or $auth['_m4_1_1']==0) {
+			if( !$auth['_m4_1_1'] or $auth['_m4_1_1']==0) { // 조회 권한이 없는 경우
 				$this->load->view('no_auth');
-			}else{
+			}else{ // 조회 권한이 있는 경우
+
+				// 불러올 페이지에 보낼 조회 권한 데이터
+				$data['auth'] = $auth['_m4_1_1'];
 
 				//본 페이지 로딩
 				$this->load->view('/menu/m4/md1_sd1_v');
@@ -75,8 +80,11 @@ class M4 extends CI_Controller {
 				$this->load->view('no_auth');
 			}else{
 
+				// 불러올 페이지에 보낼 조회 권한 데이터
+				$data['auth'] = $auth['_m4_1_2'];
+
 				//본 페이지 로딩
-				$this->load->view('/menu/m4/md1_sd2_v');
+				$this->load->view('/menu/m4/md1_sd2_v', $data);
 			}
 
 
@@ -92,8 +100,11 @@ class M4 extends CI_Controller {
 				$this->load->view('no_auth');
 			}else{
 
+				// 불러올 페이지에 보낼 조회 권한 데이터
+				$data['auth'] = $auth['_m4_1_3'];
+
 				//본 페이지 로딩
-				$this->load->view('/menu/m4/md1_sd3_v');
+				$this->load->view('/menu/m4/md1_sd3_v', $data);
 			}
 
 
@@ -109,8 +120,11 @@ class M4 extends CI_Controller {
 				$this->load->view('no_auth');
 			}else{
 
+				// 불러올 페이지에 보낼 조회 권한 데이터
+				$data['auth'] = $auth['_m4_2_1'];
+
 				//본 페이지 로딩
-				$this->load->view('/menu/m4/md2_sd1_v');
+				$this->load->view('/menu/m4/md2_sd1_v', $data);
 			}
 
 
@@ -126,8 +140,13 @@ class M4 extends CI_Controller {
 				$this->load->view('no_auth');
 			}else{
 
+				// 불러올 페이지에 보낼 조회 권한 데이터
+				$data['auth'] = $auth['_m4_2_2'];
+
+				
+
 				//본 페이지 로딩
-				$this->load->view('/menu/m4/md2_sd2_v');
+				$this->load->view('/menu/m4/md2_sd2_v', $data);
 			}
 
 
@@ -143,8 +162,11 @@ class M4 extends CI_Controller {
 				$this->load->view('no_auth');
 			}else{
 
+				// 불러올 페이지에 보낼 조회 권한 데이터
+				$data['auth'] = $auth['_m4_2_3'];
+
 				//본 페이지 로딩
-				$this->load->view('/menu/m4/md2_sd3_v');
+				$this->load->view('/menu/m4/md2_sd3_v', $data);
 			}
 		}
 	}
--- application/views/menu/m4/md1_sd2_v.php
+++ application/views/menu/m4/md1_sd2_v.php
@@ -1,20 +1,67 @@
-			<div class="main_start">dd</div>
+<?php
+	if($auth<1){ $excel_pop = "alert('출력 권한이 없습니다!');";
+	}else{
+		$url_date = urlencode('$sh_date');
+		$excel_pop = "location.href='".base_url()."excel_daily_money_report.php?sh_date=".$url_date."' ";
+	}
+ ?>
+			<div class="main_start"><a href="javascript:" onclick="<?php echo $excel_pop; ?>">
+				<img src="/static/img/excel_icon.jpg" height="10" border="0" alt="EXCEL 아이콘" /> EXCEL로 출력</a>
+			</div>
+
 			<div class="row" style="margin: 0 0 20px 0; border-width: 0 0 1px 0; border-style: solid; border-color: #cccccc;">
-				<div class="col-md-2" style="background-color: #F4F4F4; height: 40px; padding-top: 10px;">부서별</div>
-				<div class="col-md-10" style="height: 40px; padding-top: 10px;">
-					<select>
-						<option>전 체</option>
+				<div class="col-md-1 center" style="background-color: #F4F4F4; height: 40px; padding-top: 10px;">구 분</div>
+				<div class="col-md-1" style="height: 40px; padding: 5px;">
+					<select class="form-control input-sm wid-100">
+						<option value="">전 체</option>
+						<option value="">입금</option>
+						<option value="">출금</option>
+						<option value="">대체</option>
+					</select>
+				</div>
+				<div class="col-md-1" style="height: 40px; padding: 5px;">
+					<select class="form-control input-sm wid-100">
+						<option value="">전 체</option>
 						<option value=""></option>
 						<option value=""></option>
 						<option value=""></option>
 						<option value=""></option>
 					</select>
 				</div>
+				<div class="col-md-1 center" style="background-color: #F4F4F4; height: 40px; padding-top: 10px;">거래기간</div>
+				<div class="col-md-3" style="height: 40px; padding: 5px;">
+					<div class="col-xs-5" style="padding: 0px;">
+						<input type="text" class="form-control input-sm wid-95" id="es_date" name="es_date" maxlength="10" value="" readonly onClick="cal_add(this); event.cancelBubble=true" placeholder="시작일">
+					</div>
+					<div class="col-xs-1 glyphicon-wrap" style="padding: 6px 0;">
+						<a href="javascript:" onclick="cal_add(document.getElementById('es_date'),this); event.cancelBubble=true">
+							<span class="glyphicon glyphicon-calendar" aria-hidden="true" id="glyphicon"></span>
+						</a>
+					</div>
+					<div class="col-xs-5" style="padding: 0px;">
+						<input type="text" class="form-control input-sm wid-95" id="es_date" name="es_date" maxlength="10" value="" readonly onClick="cal_add(this); event.cancelBubble=true" placeholder="종료일">
+					</div>
+					<div class="col-xs-1 glyphicon-wrap" style="padding: 6px 0;">
+						<a href="javascript:" onclick="cal_add(document.getElementById('es_date'),this); event.cancelBubble=true">
+							<span class="glyphicon glyphicon-calendar" aria-hidden="true" id="glyphicon"></span>
+						</a>
+					</div>
+				</div>
+				<div class="col-md-2">
+					<img src="/static/images/to_today.jpg" alt="">
+				</div>
+				<div class="col-md-1 center" style="background-color: #F4F4F4; height: 40px; padding: 5px;">
+					<select class="form-control input-sm" name="">
+						<option value="">통합검색</option>
+					</select>
+				</div>
+				<div class="col-md-1" style="height: 40px; padding: 5px 0 0 5px;"><input type="text" name="name" value="" class="form-control input-sm"></div>
+				<div class="col-md-1 center" style="height: 40px; padding: 3px;"><input type="button" name="name" value="검 색" class="btn btn-primary btn-sm"></div>
 			</div>
 			<div class="row">
 				<div class="col-md-12 table-responsive" style="<?php if( !$this->agent->is_mobile()) echo 'height: 432px;'; ?>">
 					<!-- <div class="center" style="padding-top: 100px;">등록된 데이터가 없습니다.</div> -->
-					<table class="table table-bordered table-condensed">
+					<table class="table table-condensed">
 						<thead>
 							<tr>
 								<th class="col-md-1 center"><input type="checkbox"></th>
