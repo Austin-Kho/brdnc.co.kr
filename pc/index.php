56fedc9c48602212034d6903fca7c53dcf6d4d8d
CHANG-KYUN KHO <CHANG-KYUN KHO>
2016-04-25 14:31:27 +0900

    메인 인덱스 화면 변경

--- pc/index.php
+++ pc/index.php
@@ -3,7 +3,7 @@
   if(file_exists("setup/config_set.php")){
 		 echo "<table align='center' width='500' height='200' border='0' cellpadding='0' cellspacing='0'>
 		 <tr>
-			<td align='center' valign='middle'><meta http-equiv='Refresh' content='0; URL=/cms/cms.php'></td>
+			<td align='center' valign='middle'><meta http-equiv='Refresh' content='0; URL=/pc/cms.php'></td>
 		 </tr>
 		 </table>";
 	}else{
