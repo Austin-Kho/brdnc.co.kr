1c9f5cdff46d7bf10423bb8abd9fa94154e0feaa
cigiko <cigiko@naver.com>
2016-05-16 00:47:03 +0900

    모바일 비율 고정 소스 추가

--- ns/application/views/cms_main_header.php
+++ ns/application/views/cms_main_header.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 		<meta name="description" content="(주)바램디앤씨 관리프로그램">
 		<meta name="author" content="(주)바램디앤씨">
