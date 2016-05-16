bcca89f6f51c40f2f4781156c430cccb5663e16e
CHANG-KYUN KHO <CHANG-KYUN KHO>
2016-05-16 09:07:26 +0900

    계약 등록 페이지 UI 개선

--- ns/application/views/menu/m1/md1_sd2_v.php
+++ ns/application/views/menu/m1/md1_sd2_v.php
@@ -370,6 +370,6 @@
 		</form>
 
 <?php if($auth<2) {$submit_str="alert('등록 권한이 없습니다!')";} else {$submit_str="cont_check();";} ?>
-		<div class="form-group btn-wrap" style="margin: 30px 0 0 0;">
+		<div class="form-group btn-wrap" style="margin: <?php echo $mg = ((( !$this->input->post('cont_sort2') && !$this->input->post('cont_sort3')) OR ($this->input->post('cont_sort2')!=2 && $this->input->post('cont_sort3')!=4)))? '130px' :' 30px'; ?> 0 0 0;">
 			<input type="button" class="btn btn-primary btn-sm" onclick="<?=$submit_str?>" value="등록 하기">
 		</div>
