03f95f18e0a21fef435c489fae76d9857e361478
CHANG-KYUN KHO <CHANG-KYUN KHO>
2016-06-27 16:43:51 +0900

    약정별 수납 현황 작업 진행 중

--- ns/application/controllers/popup/Sales_receive.php
+++ ns/application/controllers/popup/Sales_receive.php
@@ -22,29 +22,43 @@ class Sales_receive extends CI_Controller
 		//본 페이지 로딩
 		$this->load->view('/popup/pop_header_v');
 
-		$project = $this->input->get('project');
+		// 조회 등록 권한 체크
+		$auth = $this->main_m->auth_chk('_m1_2_1', $this->session->userdata['user_id']);
 
-		// 납부 회차 데이터
-		$pay_sche = $data['pay_sche'] = $this->main_m->sql_result( "SELECT seq, pay_name FROM  cms_sales_pay_sche WHERE pj_seq='$project' ORDER BY pay_code " );
+		if( !$auth['_m1_2_1'] or $auth['_m1_2_1']==0) {
+			$this->load->view('no_auth');
+		}else{
+			// 불러올 페이지에 보낼 조회 권한 데이터
+			$data['auth'] = $auth['_m1_2_1'];
 
-		// price seq 전체 가져오기
-		$price = $data['price'] = $this->main_m->sql_result(" SELECT seq, unit_price FROM cms_sales_price WHERE pj_seq='$project' ORDER BY seq ");
+			// 등록된 프로젝트 데이터
+			$where = "";
+			if($this->input->get('yr') !="") $where=" WHERE biz_start_ym LIKE '".$this->input->get('yr')."%' ";
+			$data['all_pj'] = $this->main_m->sql_result(' SELECT * FROM cms_project '.$where.' ORDER BY biz_start_ym DESC ');
+			$project = $data['project'] = ($this->input->get('project')) ? $this->input->get('project') : 1; // 선택한 프로젝트 고유식별 값(아이디)
 
-		//계약 통계 계산
-		$data['total_sum'] = 0;
-		$data['sche_sum'] = 0;
-		for ($i=0; $i<count($price); $i++) {
-			$cont_sum = $this->main_m->sql_row(" SELECT COUNT(seq) AS num FROM cms_sales_contract WHERE price_seq='".$price[$i]->seq."' ");
-			$data['total_sum'] += $price[$i]->unit_price*$cont_sum->num; // 현재 분양 총 매출액
+			// 납부 회차 데이터
+			$pay_sche = $data['pay_sche'] = $this->main_m->sql_result( "SELECT seq, pay_name FROM  cms_sales_pay_sche WHERE pj_seq='$project' ORDER BY pay_code " );
+
+			// price seq 전체 가져오기
+			$price = $data['price'] = $this->main_m->sql_result(" SELECT seq, unit_price FROM cms_sales_price WHERE pj_seq='$project' ORDER BY seq ");
+
+			//계약 통계 계산
+			$data['total_sum'] = 0;
+			$data['sche_sum'] = 0;
+			for ($i=0; $i<count($price); $i++) {
+				$cont_sum = $this->main_m->sql_row(" SELECT COUNT(seq) AS num FROM cms_sales_contract WHERE price_seq='".$price[$i]->seq."' ");
+				$data['total_sum'] += $price[$i]->unit_price*$cont_sum->num; // 현재 분양 총 매출액
+			}
+
+			//본 페이지 로딩
+			$this->load->view('/popup/sales_receive_v', $data);
+
+
+
+			//본 페이지 로딩
+			$this->load->view('/popup/pop_footer_v');
 		}
-
-		//본 페이지 로딩
-		$this->load->view('/popup/sales_receive_v', $data);
-
-
-
-		//본 페이지 로딩
-		$this->load->view('/popup/pop_footer_v');
 	}
 }
 // End of File.
--- ns/application/views/popup/sales_receive_v.php
+++ ns/application/views/popup/sales_receive_v.php
@@ -6,8 +6,9 @@
 				<label for="view_sort" class="sr-only">프로젝트 선택</label>
 				<select class="form-control input-sm" name="view_sort" onchange="submit();" style="margin: 5px 0;">
 					<option value="0"> 전 체
-					<option value="1" <?php if( !$this->input->get('view_sort') OR $this->input->get('view_sort')=='1') echo "selected"; ?>>월 별</option>
-					<option value="2" <?php if($this->input->get('view_sort')=='2') echo "selected"; ?>>일 별</option>
+<?php foreach($all_pj as $lt) : ?>
+							<option value="<?php echo $lt->seq; ?>" <?php if(( !$this->input->post('project') && $lt->seq=='1') OR $this->input->get('project')==$lt->seq) echo "selected"; ?>><?php echo $lt->pj_name; ?>
+<?php endforeach; ?>
 				</select>
 			</div>
 			<div class="col-xs-8 col-sm-5 col-md-1 right" style="padding-top: 11px;"><span class="glyphicon glyphicon-file" aria-hidden="true"> 분양구분 : </div>
