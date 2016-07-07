65d3b8fb2d2193325c0c20e8671e5e8e23108209
CHANG-KYUN KHO <CHANG-KYUN KHO>
2016-07-07 17:57:46 +0900

    최근 입금 현황에 입금자명 외 계약자명 데이터 표시 추가

--- ns/application/views/menu/m1/md2_sd1_v.php
+++ ns/application/views/menu/m1/md2_sd1_v.php
@@ -185,6 +185,7 @@ echo form_open(base_url(uri_string()), $attributes);
 						<td>납입회차</td>
 						<td>입금계좌</td>
 						<td>당 건 총입금액</td>
+						<td>계약자</td>
 						<td>타입</td>
 						<td>동호수</td>
 					</tr>
@@ -201,7 +202,8 @@ endfor;
 
 foreach($rec_list as $lt) :
 	$dong_ho = explode("-", $lt->unit_dong_ho);
-	$total_rec = $this->main_m->sql_row(" SELECT SUM(paid_amount) AS pa FROM cms_sales_received WHERE pj_seq='$project' AND cont_seq='$lt->cont_seq' GROUP BY cont_seq ");
+	$contractor = $this->main_m->sql_row(" SELECT contractor AS ct FROM cms_sales_contractor WHERE cont_seq='$lt->cont_seq' ");
+	$total_rec = $this->main_m->sql_row(" SELECT SUM(paid_amount) AS pa FROM cms_sales_received WHERE pj_seq='$project' AND cms_sales_received.cont_seq='$lt->cont_seq' GROUP BY cms_sales_received.cont_seq ");
 ?>
 					<tr class="center">
 						<td><?php echo $lt->paid_date; ?></td>
@@ -210,6 +212,7 @@ foreach($rec_list as $lt) :
 						<td><?php echo $lt->pay_name; ?></td>
 						<td><?php echo $lt->acc_nick; ?></td>
 						<td class="right"><a href="<?php echo  base_url('m1/sales/2/2').'?modi=1&project='.$project.'&dong='.$dong_ho[0].'&ho='.$dong_ho[1].'&rec_seq='.$lt->seq; ?>"><?php echo number_format($total_rec->pa); ?></a></td>
+						<td><b><?php echo $contractor->ct; ?></b></td>
 						<td class="left"><span style="background-color: <?php echo $type_color[$lt->unit_type]; ?>">&nbsp;&nbsp;&nbsp;&nbsp;</span> <?php echo $lt->unit_type; ?></td>
 						<td><a href="<?php echo base_url('m1/sales/1/2')."?mode=1&cont_sort1=1&cont_sort2=2&project=".$project."&type=".$lt->unit_type."&dong=".$dong_ho[0]."&ho=".$dong_ho[1]; ?>"><?php echo $lt->unit_dong_ho; ?></a></td>
 					</tr>
