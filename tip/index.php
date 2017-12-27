b89f24282d25bb53a23fd6ccf5cb8c9cc5d1736a
Austin-Kho(w) <kori.susie@gmail.com>
2017-12-27 12:47:15 +0900

    경미한 변경

--- tip/index.php
+++ tip/index.php
@@ -7,6 +7,6 @@
   <title>My Tips Page</title>
 </head>
 <body>
-	<a href="vi.php" target="self">1. vi</a>
+	<a href="http://cigiko.cafe24.com/vi-%EC%97%90%EB%94%94%ED%84%B0/" target="self">1. vi</a>
 </body>
 </html>
