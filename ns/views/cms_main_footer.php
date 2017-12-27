78eccaaa21b9f578e1af260400703afc160fe5b1
Austin-Kho(m) <kori.susie@gmail.com>
2017-12-28 04:13:52 +0900

    푸터 다음 우편번호 경미한 변경

--- ns/views/cms_main_footer.php
+++ ns/views/cms_main_footer.php
@@ -20,8 +20,7 @@
 		      	$('[data-toggle="tooltip"]').tooltip();
 				$('[data-toggle="popover"]').popover();
 			});
-			// 다음 우편번호 서비스 -3
-			// 우편번호 찾기 화면을 넣을 element
+			// 다음 우편번호 서비스 -3 --/ 우편번호 찾기 화면을 넣을 element
 			var element_layer = document.getElementById('layer');
 		</script>
 	</body>
