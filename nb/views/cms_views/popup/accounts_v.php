3f5f34a61c8692ae99264ab47934e65514adc8f3
Austin-Kho(w) <kori.susie@gmail.com>
2018-01-08 18:27:45 +0900

    회계계정보기 팝업 완료

--- nb/views/cms_views/popup/accounts_v.php
+++ nb/views/cms_views/popup/accounts_v.php
@@ -1,9 +1,12 @@
 <script type="text/javascript">
-$(document).ready(function(){
-	$("#acc1_h").click(function(){
-			$("#acc1").toggle();
-	});
-});
+		function add_toggle(n){
+			var obj = eval(document.getElementById('acc'+n));
+			if(obj.style.display=='none') {
+				obj.style.display = '';
+			}else{
+				obj.style.display = 'none';
+			}
+		}
 </script>
 	<div class="container">
 <?php
@@ -17,7 +20,7 @@ $(document).ready(function(){
 			<div class="desc">&nbsp;</div>
 			<div class="well" style="padding: 13px; margin-bottom: 20px;">※ 검색할 계정과목 명칭을 선택하여 주십시요.</div>
 				<div class="row" style="padding-top:0;">
-					<div class="form-group <?php if(is_mobile()) echo 'col-xs-6'; else echo 'col-xs-3'; ?>">
+					<!-- <div class="form-group <?php if(is_mobile()) echo 'col-xs-6'; else echo 'col-xs-3'; ?>">
 						<label id="doro_name" for="acc_d1">계정과목 [대분류] :</label>
 					</div>
 					<div class="form-group <?php if(is_mobile()) echo 'col-xs-6'; else echo 'col-xs-3'; ?>" style="border-top: 0;">
@@ -30,17 +33,19 @@ $(document).ready(function(){
 								<option value="5" <?php echo set_select('acc_d1', '5');?>> 비 용
 							</select>
 						</div>
-					</div>
+					</div> -->
 					<div class="form-group <?php if(is_mobile()) echo 'col-xs-6'; else echo 'col-xs-3'; ?>">
-						<label>희귀 계정과목 표시 : &nbsp;&nbsp;&nbsp;<input type="checkbox" name="is_sp" value="1" <?php echo set_checkbox('is_sp', '1'); ?> onClick="submit();" <?php if($this->input->post_get('is_sp')!=='') echo 'checked'; ?>></label>
+						<label>희귀 계정과목 표시 : &nbsp;&nbsp;&nbsp;
+							<input type="checkbox" name="is_sp" value="1" <?php echo set_checkbox('is_sp', '1'); ?> onClick="submit();" <?php // if($this->input->post_get('is_sp')=='on') echo 'checked'; ?>>
+						</label>
 					</div>
 				</div>
 				<div class="mt10">
 					<div class="desc">&nbsp;</div>
 				</div>
-				<div class="sub_header" id="acc1_h"><h5>자 산 계 정</h5></div>
+				<div class="sub_header" id="acc1_handler" onclick="add_toggle(1);"><h5>자 산 계 정</h5></div>
 				<div class="tb-h5">
-					<table id="acc1" class="table table-bordered table-hover table-condensed" style="display:<?if($this->input->post_get('acc_d1')==='1' or empty($this->input->post_get('acc_d1'))) echo ''; else echo 'none';?>">
+					<table id="acc1" class="table table-bordered table-hover table-condensed" style="display: <?// if($this->input->post_get('acc_d1')==='1' or empty($this->input->post_get('acc_d1'))) echo ''; else echo 'none';?>">
 <?php
 	foreach($d2_acc_1 as $lt1) : // 1-d2 계정 나열 시작
 ?>
@@ -62,9 +67,9 @@ endforeach; //1-d2 계정 나열 종료
 ?>
 					</table>
 				</div>
-				<div class="sub_header" onClick="acc_d2_show(2);"><h5>부 채 계 정</h5></div>
+				<div class="sub_header" onclick="add_toggle(2);"><h5>부 채 계 정</h5></div>
 				<div class="tb-h5">
-					<table id="acc2" class="table table-bordered table-hover table-condensed" style="display:<?if($this->input->post_get('acc_d1')==='2') echo ''; else echo 'none';?>">
+					<table id="acc2" class="table table-bordered table-hover table-condensed" style="display:none;<?// if($this->input->post_get('acc_d1')==='2') echo ''; else echo 'none';?>">
 <?php
 	foreach($d2_acc_2 as $lt2) : // 2-d2 계정 나열 시작
 ?>
@@ -86,9 +91,9 @@ endforeach;  //2-d2 계정 나열 종료
 ?>
 					</table>
 				</div>
-				<div class="sub_header" onClick="acc_d2_show(3);"><h5>자 본 계 정</h5></div>
+				<div class="sub_header" onclick="add_toggle(3);"><h5>자 본 계 정</h5></div>
 				<div class="tb-h5">
-					<table id="acc3" class="table table-bordered table-hover table-condensed" style="display:<?if($this->input->post_get('acc_d1')==='3') echo ''; else echo 'none';?>">
+					<table id="acc3" class="table table-bordered table-hover table-condensed" style="display:none;<?// if($this->input->post_get('acc_d1')==='3') echo ''; else echo 'none';?>">
 <?php
 	foreach($d2_acc_3 as $lt3) : // 3-d2 계정 나열 시작
 ?>
@@ -110,9 +115,9 @@ endforeach;  // 3-d2 계정 나열 종료
 ?>
 					</table>
 				</div>
-				<div class="sub_header" onClick="acc_d2_show(4);"><h5>수 익 계 정</h5></div>
+				<div class="sub_header" onclick="add_toggle(4);"><h5>수 익 계 정</h5></div>
 				<div class="tb-h5">
-					<table id="acc4" class="table table-bordered table-hover table-condensed" style="display:<?if($this->input->post_get('acc_d1')==='4') echo ''; else echo 'none';?>">
+					<table id="acc4" class="table table-bordered table-hover table-condensed" style="display:none;<?// if($this->input->post_get('acc_d1')==='4') echo ''; else echo 'none';?>">
 <?php
 	foreach($d2_acc_4 as $lt4) : // 4-d2 계정 나열 시작
 ?>
@@ -134,9 +139,9 @@ endforeach;  // 4-d2 계정 나열 종료
 ?>
 					</table>
 				</div>
-				<div class="sub_header" onClick="acc_d2_show(5);"><h5>비 용 계 정</h5></div>
+				<div class="sub_header" onclick="add_toggle(5);"><h5>비 용 계 정</h5></div>
 				<div class="tb-h5">
-					<table id="acc5" class="table table-bordered table-hover table-condensed" style="display:<?if($this->input->post_get('acc_d1')==='5') echo ''; else echo 'none';?>">
+					<table id="acc5" class="table table-bordered table-hover table-condensed" style="display:none;<?// if($this->input->post_get('acc_d1')==='5') echo ''; else echo 'none';?>">
 <?php
 	foreach($d2_acc_5 as $lt5) : // 5-d2 계정 나열 시작
 ?>
