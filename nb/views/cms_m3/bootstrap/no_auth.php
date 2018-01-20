7579775c77240aca420dab35f6b8e2991a850bff
Austin-Kho(m) <kori.susie@gmail.com>
2018-01-20 23:59:43 +0900

    권한없음 페이지 UI개선

--- nb/views/cms_m3/bootstrap/no_auth.php
+++ nb/views/cms_m3/bootstrap/no_auth.php
@@ -1,4 +1,5 @@
-		<div class="container" style="text-align: center; padding-top: 50px; <?php if( !$this->agent->is_mobile()) echo 'height: 620px;'?>">
+		<div class="col-xs-12" style="text-align: center; padding-top: 50px;
+		<?php if( !$this->agent->is_mobile()) echo 'height: 520px;'?>">
 			<div class="page-header">
 				<h3>해당 페이지에 대한 조회 / 관리 권한이 없습니다.</h3>
 			</div>
