1e96d132225a1918e28877b53b4e8b7b6e063784
Austin-Kho(m) <kori.susie@gmail.com>
2018-01-21 12:15:30 +0900

    일부 세부메뉴 변경

--- nb/views/cms_m4/bootstrap/capital_v.php
+++ nb/views/cms_m4/bootstrap/capital_v.php
@@ -3,7 +3,7 @@
         	<a href="<?php echo $this->config->base_url('cms_m4/capital/1'); ?>"><strong>자 금 관 리</strong></a>
         </li>
         <li role="presentation" class="<?if( $this->uri->segment(3)=='2') echo 'active'; else echo '';?>">
-        	<a href="<?php echo $this->config->base_url('cms_m4/capital/2'); ?>"><strong>인 사 총 무</strong></a>
+        	<a href="<?php echo $this->config->base_url('cms_m4/capital/2'); ?>"><strong>일 반 관 리</strong></a>
         </li>
       </ul>
 <!-- ---------------------------------mdi-menu end------------------------------------ -->
