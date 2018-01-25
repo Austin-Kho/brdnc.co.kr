b113e4d1dfdb28c5abf2ae38ae238bddc696b7d6
Austin-Kho(w) <kori.susie@gmail.com>
2018-01-25 18:55:00 +0900

    수납고지서 - pdf 작업 진행 중

--- nb/application/controllers/cms_download/Bill_issue.php
+++ nb/application/controllers/cms_download/Bill_issue.php
@@ -7,13 +7,30 @@ class Bill_issue extends CB_Controller {
 	 */
 	public function __construct(){
 		parent::__construct();
-		// $this->load->model('cms_main_model'); //모델 파일 로드
-		// $this->load->model('cms_m4_model'); //모델 파일 로드
-		// $this->load->helper('cms_cut_string');
 	}
 
 	public function download(){
-		
+
+		// PDF를 만들기 기능을 사용하기 위해  dompdf 클래스 호출
+		include (APPPATH."third_party/dompdf/autoload.inc.php");
+		// dompdf 클래스 인스턴스화
+        $dompdf = new Dompdf\Dompdf();
+
+        $html = $this->load->view('cms_views/pdf/bill_issue',[],true);
+
+        $dompdf->loadHtml($html);
+
+        // (옵션) 용지 크기 및 방향 설정
+        $dompdf->setPaper('A4', '');
+
+        // HTML을 PDF로 렌더링
+        $dompdf->render();
+
+        // 생성 된 PDF 파일 내용 가져 오기
+        $pdf = $dompdf->output();
+
+        //  생성 된 PDF('파일명')를 브라우저로 출력하십시오.
+        $dompdf->stream('aaa');
 	}
 }
 // End of File
