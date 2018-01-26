9979de259f83c9c75356091aa60f6ecc518aa33c
Austin-Kho(w) <kori.susie@gmail.com>
2018-01-26 12:51:29 +0900

    납부고지서 수정 진행 중

--- nb/application/controllers/cms_download/Bill_issue.php
+++ nb/application/controllers/cms_download/Bill_issue.php
@@ -16,12 +16,12 @@ class Bill_issue extends CB_Controller {
 		// // url로 받은 계약 고유아이디 배열('-'로 연결을 배열로 나눔)
 		// $view['project'] = $project = $this->input->get('pj');
 		// $view['view']['cont_seq'] = explode('-', $this->input->get('seq'));
-    //
+
 		// // view page로 보낼 데이터 구하기
 		// $view['view']['bill_issue'] = $this->cms_main_model->sql_row(" SELECT * FROM cb_cms_sales_bill_issue WHERE pj_seq='$project' ");
 		// $view['view']['addr'] = explode("|", $view['view']['bill_issue']->address);
 		// $view['view']['pay_sche'] = $this->cms_main_model->sql_result(" SELECT seq, pay_sort, pay_code, pay_name, pay_due_date FROM cb_cms_sales_pay_sche WHERE pj_seq='$project' ");
-    //
+
 		// // 프로젝트명, 타입 정보 구하기
 		// $pj_info = $this->cms_main_model->sql_row(" SELECT pj_name, type_name, type_color FROM cb_cms_project WHERE seq='$project' ");
 		// if($pj_info) {
@@ -37,23 +37,23 @@ class Bill_issue extends CB_Controller {
 		$style = file_get_contents("style.css");
 
 
-    //load the view and saved it into $html variable
-    $html=$this->load->view('cms_views/pdf/bill_issue', $data, true);
+	    //load the view and saved it into $html variable
+	    $html=$this->load->view('cms_views/pdf/bill_issue', $data, true);
 
-    //this the the PDF filename that user will get to download
-    $pdfFilePath = "bill.pdf";
+	    //this the the PDF filename that user will get to download
+	    $pdfFilename = "납부_고지서.pdf";
 
 
-    //load mPDF library
-    // $this->load->library('m_pdf');
+	    //load mPDF library
+	    // $this->load->library('m_pdf');
 		$mpdf=new mPDF('ko');
 
 		$mpdf->WriteHTML($style,1);   // 1 은 스타일시트를 의미
-    //generate the PDF from the given html
-    $mpdf->WriteHTML($html, 2); // 2는 html 을 의미 //생략가능
+	    //generate the PDF from the given html
+	    $mpdf->WriteHTML($html, 2); // 2는 html 을 의미 //생략가능
 
-    //download it.
-    $mpdf->Output($pdfFilePath, "D");
+	    //download it.
+	    $mpdf->Output(iconv("UTF-8","cp949//IGNORE", $pdfFilename), "D");
 		exit;
 	}
 }
--- nb/views/cms_views/pdf/bill_issue.php
+++ nb/views/cms_views/pdf/bill_issue.php
@@ -1,25 +1,176 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>납부 고지서</title>
-    <!-- <link rel="stylesheet" type="text/css" href="style.css" /> -->
-  </head>
-  <body>
-    <table class="mytable" style="width:100%;">
+    <table style="width:100%;">
       <tr>
-        <td>동춘1구역 지역주택조합[9블록]</td>
+        <th style="height:50px; margin:auto; text-align:center;" colspan="2"><h2>동춘1구역 지역주택조합[9블록]</h2></th>
       </tr>
       <tr>
-        <td>Hello world!</td>
+        <td style="font-size:10px;">인천광역시 연수구 인천타워대로 323 B-1506 (송도동, 센트로드)</td>
+        <td style="font-size:10px; text-align:right;">Tel. 032-831-1141</td>
       </tr>
     </table>
-    <table class="mytable" style="width:100%;">
-	    <tr><th>번호</th><th>이름</th></tr>
-    	<tr><td>1</td><td>한놈</td></tr>
-    	<tr><td>2</td><td>두시기</td></tr>
-    	<tr><td>3</td><td>석삼</td></tr>
+    <table style="width:100%; font-size:11px;" cellpadding=0; cellspacing=0;>
+    	<tr>
+            <td style="border-top: 2px solid #000; padding: 8px 0 2px 15px;" width="105px;">문서번호 :</td>
+            <td style="border-top: 2px solid #000; padding: 8px 15px 2px; text-align: right;"  colspan="2">2018-01-24</td>
+        </tr>
+    	<tr>
+            <td style="padding: 5px 0 2px 15px;">수 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;신 :</td>
+            <td style="padding: 5px 0 2px;">권영기 님</td><td></td>
+        </tr>
+    	<tr>
+            <td style="padding: 5px 0 2px 15px;">참 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;조 :</td><td></td><td></td>
+        </tr>
+        <tr>
+            <td style="padding: 2px 0  8px 15px; border-bottom: 2xp solid #000;">제 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;목 :</td>
+            <td style="padding: 2px 0 8px; font-size:14px; border-bottom: 2xp solid #000;" colspan="2">동춘1구역 양우내안애 에르바체 분담금(2차 계약금 및 업무대행비) 납부 안내</td>
+        </tr>
+    </table>
+    <table style="width:100%; font-size:12px;" cellpadding=0; cellspacing=0;>
+    	<tr>
+            <td style="padding: 5px 15px;">
+                고객님의 가정에 행복과 번영이 늘 함께 하시기를 바라며 항상 동춘1구역 양우내안애 에르바체를 아껴주시는 고객님께 감사의 말씀을 전합니다.<br><br>
+                금번 아뢰올 말씀은 조합원 가입계약에 다른 분담금(2차 계약금 및 업무대행비) 납부에 대하여 안내드리오니 아래의 안내를 참고하시어 납부일정에 따라 납부하여 주시기 바랍니다.
+            </td>
+        </tr>
+    </table>
+    <table style="margin-top: 10px;"><tr><td style="font-size: 12px;">■ 계약내용</td></tr></table>
+    <table style="width:100%; font-size:11px; border-collapse:collapse;" cellpadding=0; cellspacing=0;>
+    	<tr>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">성 명</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">계 약 일</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">동 / 호수</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">평 형</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">총 분담금액</td>
+        </tr>
+        <tr>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">권영기</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">2016-04-11</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">904-1704</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">84C</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:right;">329,900,000</td>
+        </tr>
+    </table>
+    <table style="margin-top: 10px;"><tr><td style="font-size: 12px;">■ 금회차 분담금 납부 안내</td></tr></table>
+    <table style="width:100%; font-size:11px; border-collapse:collapse;" cellpadding=0; cellspacing=0;>
+    	<tr>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">납 부 회 차</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">납 부 기 한</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">금회 약정 금액</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">미납부금액</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">연체료</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">금회 납부 총액</td>
+        </tr>
+        <tr>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">권영기</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">2016-04-11</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">904-1704</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">84C</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">84C</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:right;">329,900,000</td>
+        </tr>
+        <tr>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">권영기</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">2016-04-11</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">904-1704</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">84C</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">84C</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:right;">329,900,000</td>
+        </tr>
+        <tr>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">권영기</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">2016-04-11</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">904-1704</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">84C</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">84C</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:right;">329,900,000</td>
+        </tr>
+        <tr>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;" colspan="2">합 계</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">904-1704</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">84C</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">84C</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:right;">329,900,000</td>
+        </tr>
+    </table>
+    <table style="margin-top: 10px;"><tr><td style="font-size: 12px;">■ 계좌번호 안내</td></tr></table>
+    <table style="width:100%; font-size:11px; border-collapse:collapse;" cellpadding=0; cellspacing=0;>
+    	<tr>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">구 분</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">개설은행명</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">예 금 주</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">계 좌 번 호</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">입금하실 금액</td>
+        </tr>
+        <tr>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">조합 분담금</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">KEB 하나은행(구 외환)</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">아시아신탁(주)</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">630-010178-148</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:right;">29,160,000</td>
+        </tr>
+        <tr>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">업무 대행비</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">농협</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">(주)바램디앤씨</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">355-0049-3563-43</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:right;">3,200,000</td>
+        </tr>
+        <tr>
+            <td style="padding: 5px 10px; border:1px solid black;" colspan="3">
+                ※ 계좌 입금 시 반드시 계약자명과 동호수를 표기하여 납부하여 주시기 바랍니다. <br /> 예) 홍길동901-101 or 홍길동901101
+            </td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">합 계</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:right;">32,360,000</td>
+        </tr>
     </table>
 
-  </body>
-</html>
+    <table style="margin-top: 10px;"><tr><td style="font-size: 12px;">■ 조합 분담금 약정 내역 및 납입내역</td></tr></table>
+    <table style="width:100%; font-size:11px; border-collapse:collapse;" cellpadding=0; cellspacing=0;>
+    	<tr>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;" rowspan="3">구 분</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;" colspan="2">약 정 사 항</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;" colspan="7">수 납 사 항</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;" rowspan="3">비고</td>
+        </tr>
+        <tr>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;" rowspan="2">납부기한</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;" rowspan="2">금 액</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;" colspan="2">수납원금</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;" colspan="2">할인료</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;" colspan="2">연체료</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;" rowspan="2">실 수납금액</td>
+        </tr>
+        <tr>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">일자</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">금액</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">일수</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">금액</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">일수</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">금액</td>
+        </tr>
+
+        <tr>
+            <td style="padding: 0px; border:1px solid black; height: 286px;" colspan="11">
+
+            </td>
+        </tr>
+        <tr>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">합계</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;" colspan="2">329,900,000</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;" colspan="2">32,440,000</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">-</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">-</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">-</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">-</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;">-</td>
+            <td style="padding: 3px 10px; border:1px solid black; text-align:center;"></td>
+        </tr>
+    </table>
+    <table style="width:100%; font-size:10px;" cellpadding=0; cellspacing=0;>
+    	<tr>
+            <td style="padding: 3px 0 0 10px; font-size: 11px;">업무대행사</td>
+            <td style="padding: 3px 0px; font-size: 12px;">(주)바램디앤씨</td>
+            <td style="padding: 3px 0px; font-size: 12px;">문의전화 :</td>
+            <td style="padding: 3px 0px; font-size: 12px;">032-858-9556</td>
+            <td style="padding: 3px 10px; text-align:right; font-size: 15px;">동춘1구역지역주택조합[9블록]</td>
+        </tr>
+    </table>
