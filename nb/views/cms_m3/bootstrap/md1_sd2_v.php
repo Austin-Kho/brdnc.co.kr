51795294d921e3bd250aa7e26c2899f249d235b7
Austin-Kho(m) <kori.susie@gmail.com>
2018-01-27 15:44:52 +0900

    소스개선

--- nb/views/cms_m3/bootstrap/md1_sd2_v.php
+++ nb/views/cms_m3/bootstrap/md1_sd2_v.php
@@ -4,11 +4,11 @@
   else :
 ?>
 <div class="main_start">&nbsp;</div>
-<!-- 1. 분양관리 -> 2. 수납 관리 ->2.   설정 관리 -->
+<!-- 3. 프로젝트 -> 1. 프로젝트 관리 ->2. 기타 세부설정 -->
 
 <?php
-$attributes = array('method' => 'get', 'name' => 'get_frm');
-echo form_open(current_url(), $attributes);
+  $attributes = array('method' => 'get', 'name' => 'get_frm');
+  echo form_open(current_url(), $attributes);
 ?>
   <div class="row bo-top bo-bottom font12" style="margin: 0 0 20px 0;">
     <div class="col-xs-4 col-sm-3 col-md-2 center point-sub" style="padding: 10px; 0">사업 개시년도</div>
@@ -18,10 +18,10 @@ echo form_open(current_url(), $attributes);
         <select class="form-control input-sm" name="yr" onchange="submit();">
           <option value=""> 전 체
 <?php
-$start_year = "2015";
-// if(!$yr) $yr=date('Y');  // 첫 화면에 전체 계약 목록을 보이고 싶으면 이 행을 주석 처리
-$year=range($start_year,date('Y'));
-for($i=(count($year)-1); $i>=0; $i--) :
+  $start_year = "2015";
+  // if(!$yr) $yr=date('Y');  // 첫 화면에 전체 계약 목록을 보이고 싶으면 이 행을 주석 처리
+  $year=range($start_year,date('Y'));
+  for($i=(count($year)-1); $i>=0; $i--) :
 ?>
           <option value="<?php echo $year[$i]?>" <?php if($this->input->get('yr')==$year[$i]) echo "selected"; ?>><?php echo $year[$i]."년"?>
 <?php endfor; ?>
@@ -57,36 +57,26 @@ for($i=(count($year)-1); $i>=0; $i--) :
     </div>
   </div>
 </form>
-<!--||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||-프로젝트 /조건선택 종료-|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||-->
+<!--||||||||||||||||||||||||||||||||||-프로젝트 /조건선택 종료-||||||||||||||||||||||||||||||||||-->
 
-<?php if( !$this->input->get('reg_sort') OR $this->input->get('reg_sort')==='1') : ?>
-<div class="row bo-top bo-bottom font12" style="margin: 0 0 20px;">
-  <div class="col-xs-12 font14" style="padding: 0;"><p class="bg-success" style="padding: 13px 30px; margin: 0;">1. 분양 차수 설정</p></div>
-</div>
-<?php elseif($this->input->get('reg_sort')==='2') : ?>
-<div class="row bo-top bo-bottom font12" style="margin: 0 0 20px;">
-  <div class="col-xs-12 font14" style="padding: 0;"><p class="bg-success" style="padding: 13px 30px; margin: 0;">2. 납입 회차 설정</p></div>
-</div>
-<?php elseif($this->input->get('reg_sort')==='3') : ?>
-<div class="row bo-top bo-bottom font12" style="margin: 0 0 20px;">
-  <div class="col-xs-12 font14" style="padding: 0;"><p class="bg-success" style="padding: 13px 30px; margin: 0;">3. 층별 조건 설정</p></div>
-</div>
-<?php elseif($this->input->get('reg_sort')==='4') : ?>
-<div class="row bo-top bo-bottom font12" style="margin: 0 0 20px;">
-  <div class="col-xs-12 font14" style="padding: 0;"><p class="bg-success" style="padding: 13px 30px; margin: 0;">4. 향별 조건 설정</p></div>
-</div>
-<?php elseif($this->input->get('reg_sort')==='5') : ?>
-<div class="row bo-top bo-bottom font12" style="margin: 0 0 20px;">
-  <div class="col-xs-12 font14" style="padding: 0;"><p class="bg-success" style="padding: 13px 30px; margin: 0;">5. 조건별 분양가 설정</p></div>
-</div>
-<?php elseif($this->input->get('reg_sort')==='6') : ?>
-<div class="row bo-top bo-bottom font12" style="margin: 0 0 20px;">
-  <div class="col-xs-12 font14" style="padding: 0;"><p class="bg-success" style="padding: 13px 30px; margin: 0;">6. 회차별 납입가 설정</p></div>
-</div>
-<?php endif; ?>
-<!--||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||-조건별 제목 종료-|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||-->
+<?php
+  if( !$this->input->get('reg_sort') OR $this->input->get('reg_sort')==='1') : $sub_title = "1. 분양 차수 설정";
+  elseif($this->input->get('reg_sort')==='2') :$sub_title = "2. 납입 회차 설정";
+  elseif($this->input->get('reg_sort')==='3') :$sub_title = "3. 층별 조건 설정";
+  elseif($this->input->get('reg_sort')==='4') :$sub_title = "4. 향별 조건 설정";
+  elseif($this->input->get('reg_sort')==='5') :$sub_title = "5. 조건별 분양가 설정";
+  elseif($this->input->get('reg_sort')==='6') :$sub_title = "6. 회차별 납입가 설정";
+  endif;
+?>
 
-<?php if( !$this->input->get('reg_sort') OR $this->input->get('reg_sort')==='1') : //1. 분양 차수 설정?>
+<div class="row bo-top bo-bottom font12" style="margin: 0 0 20px;">
+  <div class="col-xs-12 font14" style="padding: 0;">
+    <p class="bg-success" style="padding: 13px 30px; margin: 0;"><?php echo $sub_title; ?></p>
+  </div>
+</div>
+<!--||||||||||||||||||||||||||||||||||-조건별 제목 종료-||||||||||||||||||||||||||||||||||-->
+
+<?php if( !$this->input->get('reg_sort') OR $this->input->get('reg_sort')==='1') { //1. 분양 차수 설정?>
 <div class="row font12" style="margin: 0 0 300px;">
   <div class="col-xs-12 col-sm-12 col-md-8 table-responsive"  style="padding: 0;">
     <table class="table table-hover">
@@ -115,37 +105,36 @@ for($i=(count($year)-1); $i>=0; $i--) :
     </table>
   </div>
 </div>
-<!--||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||-1. 분양 차수 설정 종료-|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||-->
+<!--||||||||||||||||||||||||||||||||||-1. 분양 차수 설정 종료-||||||||||||||||||||||||||||||||||-->
 
-<?php elseif($this->input->get('reg_sort')==='2') : //2. 납입 회차 설정 ?>
+<?php }elseif($this->input->get('reg_sort')==='2') { //2. 납입 회차 설정 ?>
 <div class="row bo-top bo-bottom font12" style="margin: 0 0 20px;">
   <div class="col-xs-12 font14"  style="padding-bottom: 440px;">2. 납입 회차 설정</div>
 </div>
-<!--||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||-2. 납입 회차 설정 종료-|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||-->
+<!--||||||||||||||||||||||||||||||||||-2. 납입 회차 설정 종료-||||||||||||||||||||||||||||||||||-->
 
-<?php elseif($this->input->get('reg_sort')==='3') : //3. 층별 조건 설정 ?>
+<?php }elseif($this->input->get('reg_sort')==='3') { //3. 층별 조건 설정 ?>
 <div class="row bo-top bo-bottom font12" style="margin: 0 0 20px;">
   <div class="col-xs-12 font14"  style="padding-bottom: 440px;">3. 층별 조건 설정</div>
 </div>
-<!--||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||-3. 층별 조건 설정 종료-|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||-->
+<!--||||||||||||||||||||||||||||||||||-3. 층별 조건 설정 종료-||||||||||||||||||||||||||||||||||-->
 
-<?php elseif($this->input->get('reg_sort')==='4') : //4. 향별 조건 설정 ?>
+<?php }elseif($this->input->get('reg_sort')==='4') { //4. 향별 조건 설정 ?>
 <div class="row bo-top bo-bottom font12" style="margin: 0 0 20px;">
   <div class="col-xs-12 font14"  style="padding-bottom: 440px;">4. 향별 조건 설정</div>
 </div>
-<!--||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||-4. 향별 조건 설정 종료-|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||-->
+<!--||||||||||||||||||||||||||||||||||-4. 향별 조건 설정 종료-||||||||||||||||||||||||||||||||||-->
 
-<?php elseif($this->input->get('reg_sort')==='5') : //5. 조건별 분양가 설정 ?>
+<?php }elseif($this->input->get('reg_sort')==='5') { //5. 조건별 분양가 설정 ?>
 <div class="row bo-top bo-bottom font12" style="margin: 0 0 20px;">
   <div class="col-xs-12 font14"  style="padding-bottom: 440px;">5. 조건별 분양가 설정</div>
 </div>
-<!--||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||-5. 조건별 분양가 설정 종료-|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||-->
+<!--||||||||||||||||||||||||||||||||||-5. 조건별 분양가 설정 종료-||||||||||||||||||||||||||||||||||-->
 
-<?php elseif($this->input->get('reg_sort')==='6') : //6. 회차별 납입가 설정 ?>
-<!-- <form method="get" name="get_frm" action="<?php echo current_url(); ?>"> -->
-<?php
-$attributes = array('method' => 'get', 'name' => 'get_frm');
-echo form_open(current_url(), $attributes);
+<?php }elseif($this->input->get('reg_sort')==='6') { //6. 회차별 납입가 설정
+
+  $attributes = array('method' => 'get', 'name' => 'get_frm');
+  echo form_open(current_url(), $attributes);
 ?>
   <input type="hidden" name="yr" value="<?php echo $this->input->get('yr'); ?>">
   <input type="hidden" name="project" value="<?php echo $this->input->get('project'); ?>">
@@ -181,16 +170,15 @@ echo form_open(current_url(), $attributes);
 
 <?php
 if( !$this->input->get('con_diff') OR  !$this->input->get('pay_sort'))  :
-if( !$this->input->get('con_diff')) :  $msg = "차수구분을 선택하여 주십시요.";
-elseif( !$this->input->get('pay_sort')) :  $msg = "회차구분을 선택하여 주십시요.";
+  if( !$this->input->get('con_diff')) :  $msg = "차수구분을 선택하여 주십시요.";
+  elseif( !$this->input->get('pay_sort')) :  $msg = "회차구분을 선택하여 주십시요.";
 endif;
 ?>
 <div class="row font12" style="margin: 0; padding: 0;">
   <div class="col-xs-12 center" style="padding: 180px 0;"><?php echo $msg; ?></div>
 </div>
-<?php else : ?>
-<?php
-echo form_open(current_url());
+<?php else :
+  echo form_open(current_url());
 ?>
   <div class="row font12" style="margin: 0; padding: 0;">
     <div class="col-xs-12 table-responsive" style="padding: 0;">
@@ -202,14 +190,14 @@ echo form_open(current_url());
             <td width="8%">층별</td>
             <td width="10%">분양가</td>
 <?php foreach($pay_sche as $lt) :  ?>
-            <td><?php echo $lt->pay_name ?></td>
+            <td><?php if($lt->pay_disc!=='') echo $lt->pay_disc; else echo $lt->pay_name; ?></td>
 <?php endforeach; ?>
           </tr>
         </thead>
         <tbody>
 <?php for($i=0; $i<count($price); $i++) :
-$diff_td = ($i===0) ?  "<td rowspan='".($pr_row)."'>".$pr_diff[$i]->diff_name."</td>" : ""; // 차수명
-$type_td = (($pr_row-$i)%$pr_floor[0]->num_floor===0) ? "<td rowspan='".$pr_floor[0]->num_floor."'>".$price[$i]->con_type."</td>" : ""; // 타입명
+  $diff_td = ($i===0) ?  "<td rowspan='".($pr_row)."'>".$pr_diff[$i]->diff_name."</td>" : ""; // 차수명
+  $type_td = (($pr_row-$i)%$pr_floor[0]->num_floor===0) ? "<td rowspan='".$pr_floor[0]->num_floor."'>".$price[$i]->con_type."</td>" : ""; // 타입명
 ?>
           <tr>
             <?php echo $diff_td; ?>
@@ -217,8 +205,8 @@ $type_td = (($pr_row-$i)%$pr_floor[0]->num_floor===0) ? "<td rowspan='".$pr_floo
             <td><?php echo $price[$i]->floor_name; ?></td>
             <td class="right"><?php echo number_format($price[$i]->unit_price); ?></td>
 <?php
-for($j=0; $j<count($pay_sche); $j++) :
-$pmt = $this->cms_main_model->sql_row(" SELECT * FROM cb_cms_sales_payment WHERE pj_seq='$project' AND price_seq='".$price[$i]->pr_seq."' AND pay_sche_seq='".$pay_sche[$j]->seq."' ");
+  for($j=0; $j<count($pay_sche); $j++) :
+  $pmt = $this->cms_main_model->sql_row(" SELECT * FROM cb_cms_sales_payment WHERE pj_seq='$project' AND price_seq='".$price[$i]->pr_seq."' AND pay_sche_seq='".$pay_sche[$j]->seq."' ");
 ?>
             <td style="background-color: ; padding: 3px;">
               <div style="color: #B00447;"><?php echo form_error("pmt_".$price[$i]->pr_seq."-".$pay_sche[$j]->seq); ?>
@@ -240,6 +228,6 @@ $pmt = $this->cms_main_model->sql_row(" SELECT * FROM cb_cms_sales_payment WHERE
   </div>
 </form>
 
-<?php 	endif; ?>
-<!--||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||-6. 회차별 납입가 설정 종료-|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||-->
+<?php }; ?>
+<!--||||||||||||||||||||||||||||||||||-6. 회차별 납입가 설정 종료-||||||||||||||||||||||||||||||||||-->
 <?php endif ?>
