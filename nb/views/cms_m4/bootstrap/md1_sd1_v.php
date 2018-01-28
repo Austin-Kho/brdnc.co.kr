b4f8a507bd361ce9b6a04efb5831c39a6e4487ea
Austin-Kho(m) <kori.susie@gmail.com>
2018-01-29 03:43:21 +0900

    자금일보 테이블 폭 조정

--- nb/views/cms_m4/bootstrap/md1_sd1_v.php
+++ nb/views/cms_m4/bootstrap/md1_sd1_v.php
@@ -34,23 +34,24 @@
 				</div>
 			</div>
 
-			<div class="row table-responsive" style="margin: 0;">
+			<div class="row" style="margin: 0;">
+        <div class="col-xs-12 col-md-10 table-responsive">
 
-				<!-- 자금현황 테이블 시작 -->
-				<table class="table table-bordered table-hover table-condensed font12">
-					<tr bgcolor="#f2f2f9">
-						<td colspan="5">
-						<b><font color="#ee0066">▶</font> <font color="#003399">자 금 현 황</font></b> (<?php echo $sh_date?> 현재)
-						</td>
-						<td class="right">(단위 : 원)</td>
-					</tr>
-					<tr bgcolor="#f5f5f5">
-						<td class="center" colspan="2"> 구 분 </td>
-						<td class="center">전일잔액</td>
-						<td class="center">입금(증가)</td>
-						<td class="center">출금(감소)</td>
-						<td class="center">금일잔액</td>
-					</tr>
+  				<!-- 자금현황 테이블 시작 -->
+  				<table class="table table-bordered table-hover table-condensed font12">
+  					<tr bgcolor="#f2f2f9">
+  						<td colspan="5">
+  						<b><font color="#ee0066">▶</font> <font color="#003399">자 금 현 황</font></b> (<?php echo $sh_date?> 현재)
+  						</td>
+  						<td class="right">(단위 : 원)</td>
+  					</tr>
+  					<tr bgcolor="#f5f5f5">
+  						<td class="center" colspan="2"> 구 분 </td>
+  						<td class="center">전일잔액</td>
+  						<td class="center">입금(증가)</td>
+  						<td class="center">출금(감소)</td>
+  						<td class="center">금일잔액</td>
+  					</tr>
 <?php $hk_bgcolor = "color:#000099; background-color:#FCFDF2;"; ?>
 <?php for($i=0; $i<=$bank_acc['num']; $i++): // 현금계정 + 은행계좌 수 만큼 반복 한다.
 		if($i==0) $td_str="<td class='center' style='".$hk_bgcolor."'>현금</td>";
@@ -66,24 +67,24 @@
 		$balance = $cum_inc-$cum_exp; // 계정별 최종 금일 시재(잔고)
 		$y_bal = $cum_inc-$cum_exp+$date_exp-$date_inc;
 ?>
-					<tr>
-						<?php echo $td_str?>
-						<td class="center" width="185" style="<?php if($i==0) echo $hk_bgcolor?>"><?php echo $bank_acc_name; ?></td><!-- 계정 명 -->
-						<td class="right" style="<?php if($i==0) echo $hk_bgcolor?>"><?php if($i==$bank_acc['num']) echo ''; else if($y_bal==0) echo "-"; else echo number_format($y_bal);?></td> <!-- 전일 잔액 -->
-						<td class="right" style="<?php if($i==0) echo $hk_bgcolor?>"><?php if($i==$bank_acc['num']) echo ''; else if($date_inc==0) echo "-"; else echo number_format($date_inc);?></td> <!-- 당일 입금 -->
-						<td class="right" style="<?php if($i==0) echo $hk_bgcolor?>"><?php if($i==$bank_acc['num']) echo ''; else if($date_exp==0) echo "-"; else echo number_format($date_exp);?></td> <!-- 당일 출금 -->
-						<td class="right" style="<?php if($i==0) echo $hk_bgcolor?>;"><?php if($i==$bank_acc['num']) echo ''; else if($balance==0) echo "-"; else echo number_format($balance);?></td> <!-- 금일 잔액 -->
-					</tr>
+  					<tr>
+  						<?php echo $td_str?>
+  						<td class="center" width="185" style="<?php if($i==0) echo $hk_bgcolor?>"><?php echo $bank_acc_name; ?></td><!-- 계정 명 -->
+  						<td class="right" style="<?php if($i==0) echo $hk_bgcolor?>"><?php if($i==$bank_acc['num']) echo ''; else if($y_bal==0) echo "-"; else echo number_format($y_bal);?></td> <!-- 전일 잔액 -->
+  						<td class="right" style="<?php if($i==0) echo $hk_bgcolor?>"><?php if($i==$bank_acc['num']) echo ''; else if($date_inc==0) echo "-"; else echo number_format($date_inc);?></td> <!-- 당일 입금 -->
+  						<td class="right" style="<?php if($i==0) echo $hk_bgcolor?>"><?php if($i==$bank_acc['num']) echo ''; else if($date_exp==0) echo "-"; else echo number_format($date_exp);?></td> <!-- 당일 출금 -->
+  						<td class="right" style="<?php if($i==0) echo $hk_bgcolor?>;"><?php if($i==$bank_acc['num']) echo ''; else if($balance==0) echo "-"; else echo number_format($balance);?></td> <!-- 금일 잔액 -->
+  					</tr>
 <?php endfor;// 현금 / 보통예금 수만큼 반복 for문 종료 ?>
-					<tr bgcolor="#f6f6f6">
-						<td class="center" colspan="2">현금성자산 계</td>
-						<td class="right"><?php if($yd_tot==0){echo "-";}else{echo number_format($yd_tot);}?></td>
-						<td class="right"><font color="#0066ff"><?php if($td_inc==0){echo "-";}else{echo  number_format($td_inc);}?></font></td>
-						<td class="right"><font color="#ff3300"><?php if($td_exp==0){echo "-";}else{echo number_format($td_exp);}?></font></td>
-						<td class="right"><font color="#000099"><?php if($td_tot==0){echo "-";}else{echo number_format($td_tot);}?></font></td>
-					</tr>
+  					<tr bgcolor="#f6f6f6">
+  						<td class="center" colspan="2">현금성자산 계</td>
+  						<td class="right"><?php if($yd_tot==0){echo "-";}else{echo number_format($yd_tot);}?></td>
+  						<td class="right"><font color="#0066ff"><?php if($td_inc==0){echo "-";}else{echo  number_format($td_inc);}?></font></td>
+  						<td class="right"><font color="#ff3300"><?php if($td_exp==0){echo "-";}else{echo number_format($td_exp);}?></font></td>
+  						<td class="right"><font color="#000099"><?php if($td_tot==0){echo "-";}else{echo number_format($td_tot);}?></font></td>
+  					</tr>
 
-					<!-- -----------------------------------------대여금 집계 시작------------------------------------ -->
+  					<!-- -----------------------------------------대여금 집계 시작------------------------------------ -->
 <?php $col_num = $jh_data['num']+1; ?>
 <?php for($i=0; $i<=$jh_data['num']; $i++):
 	if($i==0) $td_str2="<td class='center bo-bottom' rowspan='$col_num'>조합대여금</td>";
@@ -99,50 +100,51 @@
 	$jh_balance = $jh_cum_exp-$jh_cum_inc; // 계정별 최종 금일 시재(잔고)
 	$jh_y_bal = $jh_cum_exp-$jh_cum_inc+$jh_date_exp-$jh_date_inc;
 ?>
-					<tr>
-						<?php echo $td_str2; ?>
-						<td><?php echo $jh_name; ?></td><!-- 조합 명 -->
-						<td class="right"><?php if($i==$jh_data['num']) echo ''; else if($jh_y_bal==0) echo '-'; else echo number_format($jh_y_bal);?></td> <!-- 전일 대여금 잔액 -->
-						<td class="right"><?php if($i==$jh_data['num']) echo ''; else if($jh_date_exp==0) echo '-'; else echo number_format($jh_date_exp);?></td> <!-- 당일 대여금 출금 -->
-						<td class="right"><?php if($i==$jh_data['num']) echo ''; else if($jh_date_inc==0) echo '-'; else echo number_format($jh_date_inc);?></td> <!-- 당일 대여금 회수 -->
-						<td class="right"><?php if($i==$jh_data['num']) echo ''; else if($jh_balance==0) echo '-'; else echo number_format($jh_balance);?></td> <!-- 금일 대여금 잔액 -->
-					</tr>
+  					<tr>
+  						<?php echo $td_str2; ?>
+  						<td><?php echo $jh_name; ?></td><!-- 조합 명 -->
+  						<td class="right"><?php if($i==$jh_data['num']) echo ''; else if($jh_y_bal==0) echo '-'; else echo number_format($jh_y_bal);?></td> <!-- 전일 대여금 잔액 -->
+  						<td class="right"><?php if($i==$jh_data['num']) echo ''; else if($jh_date_exp==0) echo '-'; else echo number_format($jh_date_exp);?></td> <!-- 당일 대여금 출금 -->
+  						<td class="right"><?php if($i==$jh_data['num']) echo ''; else if($jh_date_inc==0) echo '-'; else echo number_format($jh_date_inc);?></td> <!-- 당일 대여금 회수 -->
+  						<td class="right"><?php if($i==$jh_data['num']) echo ''; else if($jh_balance==0) echo '-'; else echo number_format($jh_balance);?></td> <!-- 금일 대여금 잔액 -->
+  					</tr>
 <? endfor; // 조합 구하기 for 문 종료 ?>
-					<tr bgcolor="#f6f6f6">
-						<td class="center" colspan="2">
-							조합대여금 계
-						</td>
-						<td class="right"><?php  if($jh_yd_tot==0){echo "-";}else{echo number_format($jh_yd_tot);}?></td>
-						<td class="right"><font color="#ff3300"><?php if($jh_td_exp==0){echo "-";}else{echo  number_format($jh_td_exp);}?></font></td>
-						<td class="right"><font color="#0066ff"><?php if($jh_td_inc==0){echo "-";}else{echo number_format($jh_td_inc);}?></font></td>
-						<td class="right"><font color="#000099"><?php if($jh_td_tot==0){echo "-";}else{echo number_format($jh_td_tot);}?></font></td>
-					</tr>
-					<!-- -----------------------------------------대여금 집계 종료------------------------------------ -->
-				</table>
+  					<tr bgcolor="#f6f6f6">
+  						<td class="center" colspan="2">
+  							조합대여금 계
+  						</td>
+  						<td class="right"><?php  if($jh_yd_tot==0){echo "-";}else{echo number_format($jh_yd_tot);}?></td>
+  						<td class="right"><font color="#ff3300"><?php if($jh_td_exp==0){echo "-";}else{echo  number_format($jh_td_exp);}?></font></td>
+  						<td class="right"><font color="#0066ff"><?php if($jh_td_inc==0){echo "-";}else{echo number_format($jh_td_inc);}?></font></td>
+  						<td class="right"><font color="#000099"><?php if($jh_td_tot==0){echo "-";}else{echo number_format($jh_td_tot);}?></font></td>
+  					</tr>
+  					<!-- -----------------------------------------대여금 집계 종료------------------------------------ -->
+  				</table>
+        </div>
 			</div>
 
-			<div class="row table-responsive" style="margin: 0; padding-top: 5px;">
+			<div class="row" style="margin: 0; padding-top: 5px;">
+        <div class="col-xs-12 col-md-10 table-responsive">
+  				<!-- 금일 수지 현황 -->
+  				<table class="table table-bordered table-hover table-condensed font12">
+  					<tr bgcolor="#f2f2f9">
+  						<td colspan="4">
+  							<b><font color="#ee0066">▶</font> <font color="#003399">금 일 수 지 현 황</font></b> (<?php echo $sh_date?> 현재)
+  						</td>
+  						<td class="right">(단위 : 원)</td>
+  					</tr>
 
-				<!-- 금일 수지 현황 -->
-				<table class="table table-bordered table-hover table-condensed font12">
-					<tr bgcolor="#f2f2f9">
-						<td colspan="4">
-							<b><font color="#ee0066">▶</font> <font color="#003399">금 일 수 지 현 황</font></b> (<?php echo $sh_date?> 현재)
-						</td>
-						<td class="right">(단위 : 원)</td>
-					</tr>
-
-					<!-- 입금 내역 -->
-					<tr bgcolor="#f5f5f5">
-						<td colspan="5"> <b>입 금 내 역</b> </td>
-					</tr>
-					<tr bgcolor="#f8f8f3">
-						<td class="center" width="150">거 래 처</td>
-						<td class="center" width="200">적 요</td>
-						<td class="center" width="100">금 액</td>
-						<td class="center" width="100">계정과목</td>
-						<td class="center" width="200">비 고</td>
-					</tr>
+  					<!-- 입금 내역 -->
+  					<tr bgcolor="#f5f5f5">
+  						<td colspan="5"> <b>입 금 내 역</b> </td>
+  					</tr>
+  					<tr bgcolor="#f8f8f3">
+  						<td class="center" width="150">거 래 처</td>
+  						<td class="center" width="200">적 요</td>
+  						<td class="center" width="100">금 액</td>
+  						<td class="center" width="100">계정과목</td>
+  						<td class="center" width="200">비 고</td>
+  					</tr>
 <?php
 	$in_num = $da_in['num'];
 	if($in_num<2) $num=2; else $num=$in_num; // 행수 설정;
@@ -154,34 +156,34 @@
 	if(empty($da_in['result'][$i]->account)) $da_in_account = '&nbsp;'; else $da_in_account = $da_in['result'][$i]->account;
 	if(empty($da_in['result'][$i]->note)) $da_in_note = '&nbsp;'; else $da_in_note = $da_in['result'][$i]->note;
 ?>
-					<tr>
-						<td><?php echo cut_string($da_in_acc,16,""); ?></td>
-						<td><?php echo cut_string($da_in_cont,20,""); ?></td>
-						<td class="right"><?php echo $income; ?></td>
-						<td class="center"><?php echo cut_string($da_in_account,10,"")?></td>
-						<td class="center"><?php echo cut_string($da_in_note,20,"")?></td>
-					</tr>
+  					<tr>
+  						<td><?php echo cut_string($da_in_acc,16,""); ?></td>
+  						<td><?php echo cut_string($da_in_cont,20,""); ?></td>
+  						<td class="right"><?php echo $income; ?></td>
+  						<td class="center"><?php echo cut_string($da_in_account,10,"")?></td>
+  						<td class="center"><?php echo cut_string($da_in_note,20,"")?></td>
+  					</tr>
 <?php endfor; ?>
-					<tr bgcolor="#f6f6f6">
-						<td class="center" colspan="2">입 금 합 계</td>
-						<td class="right"><font color="#0066ff"><?php if($da_in_total[0]->total_inc==0){echo "-";}else{echo number_format($da_in_total[0]->total_inc);}?></font></td>
-						<td class="center"></td>
-						<td class="center"></td>
-					</tr>
+  					<tr bgcolor="#f6f6f6">
+  						<td class="center" colspan="2">입 금 합 계</td>
+  						<td class="right"><font color="#0066ff"><?php if($da_in_total[0]->total_inc==0){echo "-";}else{echo number_format($da_in_total[0]->total_inc);}?></font></td>
+  						<td class="center"></td>
+  						<td class="center"></td>
+  					</tr>
 
-					<tr><td colspan="5" style="height: 20px; background-color: white;"></td></tr>
+  					<tr><td colspan="5" style="height: 20px; background-color: white;"></td></tr>
 
-					<!-- 출금 내역 -->
-					<tr bgcolor="#f5f5f5">
-						<td colspan="5"> <b>출 금 내 역</b> </td>
-					</tr>
-					<tr bgcolor="#f8f8f3">
-						<td class="center">거래처</td>
-						<td class="center">적 요</td>
-						<td class="center">금 액</td>
-						<td class="center">계정과목</td>
-						<td class="center">비 고</td>
-					</tr>
+  					<!-- 출금 내역 -->
+  					<tr bgcolor="#f5f5f5">
+  						<td colspan="5"> <b>출 금 내 역</b> </td>
+  					</tr>
+  					<tr bgcolor="#f8f8f3">
+  						<td class="center">거래처</td>
+  						<td class="center">적 요</td>
+  						<td class="center">금 액</td>
+  						<td class="center">계정과목</td>
+  						<td class="center">비 고</td>
+  					</tr>
 <?
 $ex_num = $da_ex['num'];
 if($ex_num<4) $num = 4; else $num = $ex_num;
@@ -193,20 +195,21 @@ for($i=0;$i<=$num;$i++):
 	if(empty($da_ex['result'][$i]->account)) $da_ex_account = ''; else $da_ex_account = $da_ex['result'][$i]->account;
 	if(empty($da_ex['result'][$i]->note)) $da_ex_note = ''; else $da_ex_note = $da_ex['result'][$i]->note;
 ?>
-					<tr>
-						<td><?php echo cut_string($da_ex_acc,16,"")?></td>
-						<td><?php echo cut_string($da_ex_cont,20,"")?></td>
-						<td class="right"><?php echo $exp?></td>
-						<td class="center"><?php echo cut_string($da_ex_account,10,"")?></td>
-						<td class="center"><?php echo cut_string($da_ex_note,20,"")?></td>
-					</tr>
+  					<tr>
+  						<td><?php echo cut_string($da_ex_acc,16,"")?></td>
+  						<td><?php echo cut_string($da_ex_cont,20,"")?></td>
+  						<td class="right"><?php echo $exp?></td>
+  						<td class="center"><?php echo cut_string($da_ex_account,10,"")?></td>
+  						<td class="center"><?php echo cut_string($da_ex_note,20,"")?></td>
+  					</tr>
 <?php endfor; ?>
-					<tr bgcolor="#f6f6f6">
-						<td class="center" colspan="2">출 금 합 계</td>
-						<td class="right"><font color="#ff3300"><?php if($da_ex_total[0]->total_exp==0){echo "-";}else{echo number_format($da_ex_total[0]->total_exp); }?></font></td>
-						<td class="center"></td>
-						<td class="center"></td>
-					</tr>
-				</table>
+  					<tr bgcolor="#f6f6f6">
+  						<td class="center" colspan="2">출 금 합 계</td>
+  						<td class="right"><font color="#ff3300"><?php if($da_ex_total[0]->total_exp==0){echo "-";}else{echo number_format($da_ex_total[0]->total_exp); }?></font></td>
+  						<td class="center"></td>
+  						<td class="center"></td>
+  					</tr>
+  				</table>
+        </div>
 			</div>
 <?php endif ?>
