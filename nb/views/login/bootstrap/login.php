5da9c9eec99ce9e8a56b26b811211ae0ca131a8e
Austin-Kho(m) <kori.susie@gmail.com>
2018-02-09 23:22:19 +0900

    login box css 수정

--- nb/views/login/bootstrap/login.php
+++ nb/views/login/bootstrap/login.php
@@ -1,9 +1,10 @@
 <?php
   $this->managelayout->add_css(element('view_skin_url', $layout) . '/css/style.css');
   $mar_bottom = ($this->is_mobile === TRUE) ? "margin-top: 20px; margin-bottom: 20px" : "margin-top: 150px; margin-bottom: 200px;";
+  $sh_col = array("#d3ebc9", "#ebdcc9", "#c9d7eb", "#ebcbe8", "#ebcbd0", "#ddd", "#d5f4f1", "#ffffae", "#ecd8fa");
 ?>
 
-<div class="access col-md-4 col-md-offset-4" style="<?php echo $mar_bottom;?>; border: 2px solid #ccc; padding:25px 30px; background-color: #F6F6F6;">
+<div class="access col-md-4 col-md-offset-4" style="<?php echo $mar_bottom;?>; border: 2px solid #ccc; padding:25px 30px; background-color: #F6F6F6; box-shadow: 10px 10px 100px <?php echo $sh_col[rand(0, 8)];?>;">
 
   <!-- <div class="panel panel-info"> -->
     <div class="panel-heading"><h3 style="margin-bottom: 0; font-size: 22px;">로그인</h3></div>
