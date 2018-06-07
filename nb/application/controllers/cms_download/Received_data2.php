66c0c4adb5ec57d5dd4b167703b00691e88d37e6
Austin-Kho(w) <kori.susie@gmail.com>
2018-06-07 10:15:19 +0900

    수납내역 엑셀다운로드(대용량버전) 완료

--- nb/application/controllers/cms_download/Received_data2.php
+++ nb/application/controllers/cms_download/Received_data2.php
@@ -20,22 +20,12 @@ class Received_data2 extends CB_Controller {
 
 	public function excel_file(){
 
-		$project = urldecode($this->input->get('pj'));
+		$data['pj_seq'] = urldecode($this->input->get('pj'));
 		$rec_query = urldecode($this->input->get('qry'));
 		$rec_query .= "ORDER BY paid_date, cb_cms_sales_received.seq ";
 
-		$data['project'] = $this->cms_main_model->data_row("cb_cms_project", array('seq'=>$project), "pj_name"); // 수납 데이터
 		$data['rec_data'] = $this->cms_main_model->sql_result($rec_query); // 수납 데이터
-
-
-		// $data['where'] = $this->input->get('add_where');
-		// $data['sc'] = $this->input->get('sc');
-
-		// sql문에 적용할 테이블명 ////////////////
-		// $cb_table = 'cb_cms_capital_cash_book, cb_cms_capital_bank_account';
-
-		// $data['com_title'] = $this->cms_main_model->data_row('cb_cms_com_info', array(1=>1));
-		// $data['cash_book_list'] = $this->cms_m4_model->cash_book_list($cb_table, $data['where'], '', '', '', 'ASC');
+		$data['project'] = $this->cms_main_model->data_row("cb_cms_project", array('seq'=>$data['pj_seq']), "pj_name"); // 수납 데이터
 
 		// 실제 엑셀 VIEW 파일 로드
 		$this->load->view('/cms_views/excel/received_data', $data);
--- nb/views/cms_views/excel/received_data.php
+++ nb/views/cms_views/excel/received_data.php
@@ -6,15 +6,13 @@ Header("Content-Description: PHP7 Generated Data" );
 Header("Pragma: no-cache");
 Header("Expires: 0");
 
-// $company = ($com_title->co_name===NULL) ? "회사" : $com_title->co_name;
-
 // 테이블 상단 만들기
 echo $EXCEL_STR1 = "
   <table border='1'>
     <tr align='center' height='48'>
-      <td colspan='10' style='font-size:15pt; text-align:center;'><b>".$project->pj_name." 수납 데이터</b></td>
+      <td colspan='11' style='font-size:15pt; text-align:center;'><b>".$project->pj_name." 수납 데이터</b></td>
     </tr>
-    <tr><td  colspan='10' align='right' height='20' style='font-size:9pt;'>".date('Y-m-d')." 현재</td></tr>
+    <tr><td  colspan='11' align='right' height='20' style='font-size:9pt;'>".date('Y-m-d')." 현재</td></tr>
     <tr align='center' height='20' style='font-size:9pt;'>
       <td width='50' bgcolor='#EAEAEA'>no.</td>
       <td width='90' bgcolor='#EAEAEA'>수납 일자</td>
@@ -22,10 +20,11 @@ echo $EXCEL_STR1 = "
       <td width='120' bgcolor='#EAEAEA'>입금자</td>
       <td width='100' bgcolor='#EAEAEA'>납입 회차</td>
       <td width='100' bgcolor='#EAEAEA'>입금 계좌</td>
-      <td width='110' bgcolor='#EAEAEA'>당 건 총입금액</td>
+      <td width='80' bgcolor='#EAEAEA'>일련번호</td>
       <td width='90' bgcolor='#EAEAEA'>계약자</td>
       <td width='70' bgcolor='#EAEAEA'>타입</td>
       <td width='100' bgcolor='#EAEAEA'>동호수</td>
+      <td width='110' bgcolor='#EAEAEA'>당 건 총입금액</td>
     </tr>
   ";
 
@@ -33,6 +32,12 @@ $i = 1;
 // 데이터 가져오기 시작
 foreach ($rec_data as $lt) :
   $dong_ho = explode("-", $lt->unit_dong_ho);
+
+  $code = $this->cms_main_model->sql_row(
+    " SELECT cont_code
+      FROM cb_cms_sales_contract
+      WHERE pj_seq='$pj_seq' AND seq='$lt->cont_seq' "
+  );
   $contractor = $this->cms_main_model->sql_row(
     " SELECT contractor AS ct
       FROM cb_cms_sales_contractor
@@ -41,7 +46,7 @@ foreach ($rec_data as $lt) :
   $total_rec = $this->cms_main_model->sql_row(
     " SELECT SUM(paid_amount) AS pa
       FROM cb_cms_sales_received
-      WHERE pj_seq='$project' AND cb_cms_sales_received.cont_seq='$lt->cont_seq'
+      WHERE pj_seq='$pj_seq' AND cb_cms_sales_received.cont_seq='$lt->cont_seq'
       GROUP BY cb_cms_sales_received.cont_seq "
   );
 
@@ -49,14 +54,15 @@ echo $EXCEL_STR2 = "
 <tr   height='20' style='font-size:9pt;'>
 	 <td align='center'>".$i."</td>
 	 <td align='center'>".$lt->paid_date."</td>
-	 <td align='right'>".$lt->paid_amount."</td>
+	 <td align='right'>".number_format($lt->paid_amount)."</td>
 	 <td align='center'>".$lt->paid_who."</td>
 	 <td align='center'>".$lt->pay_name."</td>
 	 <td align='center'>".$lt->acc_nick."</td>
-	 <td align='right'>".$total_rec->pa."</td>
+   <td align='center'>".$code->cont_code."</td>
 	 <td align='center'>".$contractor->ct."</td>
 	 <td align='center'>".$lt->unit_type."</td>
 	 <td align='center'>".$lt->unit_dong_ho."</td>
+   <td align='right'>".number_format($total_rec->pa)."</td>
 </tr>
    ";
    $i++;
