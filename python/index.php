cc9f8580ae647f0cb1008d071aae469c68c431ec
Austin-Kho <kori.susie@gmail.com>
2018-06-18 10:31:31 +0900

    파이썬 요약 정리 레이아웃 디자인 중

--- python/index.php
+++ python/index.php
@@ -4,25 +4,35 @@
     <meta charset="utf-8">
     <title>파이썬으로 지루한 작업 자동화 하기</title>
     <style media="screen">
-      section {
+      h2 {padding-left: 10px;}
+      h4 {padding-left: 20px;}
+      h4 {padding-left: 30px;}
+      /* section {
         padding: 10px 20px;
         background-color: #fbf6e0;
       }
       code {
         width: inherit;
         padding: 8px;
-        background-color: #e6e9ec;
-      }
+        background-color: #e0edfb;
+      } */
     </style>
+    <!-- 합쳐지고 최소화된 최신 CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+    <!-- 부가적인 테마 -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+    <!-- 합쳐지고 최소화된 최신 자바스크립트 -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+
   </head>
   <body>
     <h2>1부 파이썬 프로그래밍 기초</h2>
 
-    <h3>1장 파이썬 기초</h3>
-    <h3>2장 흐름 제어</h3>
-    <h3>3장 함수</h3>
-    <h3>4장 리스트</h3>
-    <h3>5장 사전 및 구조화 데이터</h3>
+    <h4>1장 파이썬 기초</h4>
+    <h4>2장 흐름 제어</h4>
+    <h4>3장 함수</h4>
+    <h4>4장 리스트</h4>
+    <h4>5장 사전 및 구조화 데이터</h4>
       <section>
         <article class="">
           <h5>사전 데이터 유형</h5>
@@ -34,24 +44,35 @@
         </article>
       </section>
 
-    <h3>6장 문자열 조작하기</h3>
+      <section>
+        <article class="">
+          <h5>관련 메소드</h5>
+          <ul>
+            <li>keys() : 사전명.keys() 형식으로 사용하며 해당 사전의 key 데이터를 추출한다.</li>
+            <li>values() : 사전명.values() 형식으로 사용하며 해당 사전의 value 데이터를 추출한다.</li>
+            <li>items() :  사전명.items() 형식으로 사용하며 해당 사전의 key와 value를 </li>
+          </ul>
+        </article>
+      </section>
+
+    <h4>6장 문자열 조작하기</h4>
 
 
     <p>&nbsp;</p>
     <h2>2부 작업 자동화하기</h2>
 
-    <h3>7장 정규표현식을 사용한 패턴 대조</h3>
-    <h3>8장 파일 읽기 및 쓰기</h3>
-    <h3>9장 파일 조직화하기</h3>
-    <h3>10장 디버깅</h3>
-    <h3>11장 웹 스크랩</h3>
-    <h3>12장 엑셀 스프레드시트 다루기</h3>
-    <h3>13장 PDF 및 워드 문서 작업</h3>
-    <h3>14장 CSV 파일 및 JSON 데이터 작업</h3>
-    <h3>15장 시간 지키기, 작업 예약하기 및 프로그램 실행시키기</h3>
-    <h3>16장 전자메일 및 문자 메시지 전송</h3>
-    <h3>17장 이미지 조작</h3>
-    <h3>18장 키보드와 마우스 제어 및 GUI 자동화</h3>
+    <h4>7장 정규표현식을 사용한 패턴 대조</h4>
+    <h4>8장 파일 읽기 및 쓰기</h4>
+    <h4>9장 파일 조직화하기</h4>
+    <h4>10장 디버깅</h4>
+    <h4>11장 웹 스크랩</h4>
+    <h4>12장 엑셀 스프레드시트 다루기</h4>
+    <h4>13장 PDF 및 워드 문서 작업</h4>
+    <h4>14장 CSV 파일 및 JSON 데이터 작업</h4>
+    <h4>15장 시간 지키기, 작업 예약하기 및 프로그램 실행시키기</h4>
+    <h4>16장 전자메일 및 문자 메시지 전송</h4>
+    <h4>17장 이미지 조작</h4>
+    <h4>18장 키보드와 마우스 제어 및 GUI 자동화</h4>
 
   </body>
 </html>
