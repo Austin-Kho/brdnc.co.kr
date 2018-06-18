11e33b806eb09727e091eb6bbcfe4485ef77d328
Austin-Kho <kori.susie@gmail.com>
2018-06-18 10:11:42 +0900

    파이썬 summary

---
+++ python/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="ko" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>파이썬으로 지루한 작업 자동화 하기</title>
+    <style media="screen">
+      section {
+        padding: 10px 20px;
+        background-color: #fbf6e0;
+      }
+      code {
+        width: inherit;
+        padding: 8px;
+        background-color: #e6e9ec;
+      }
+    </style>
+  </head>
+  <body>
+    <h2>1부 파이썬 프로그래밍 기초</h2>
+
+    <h3>1장 파이썬 기초</h3>
+    <h3>2장 흐름 제어</h3>
+    <h3>3장 함수</h3>
+    <h3>4장 리스트</h3>
+    <h3>5장 사전 및 구조화 데이터</h3>
+      <section>
+        <article class="">
+          <h5>사전 데이터 유형</h5>
+          <p>리스트와 마찬가지로 사전(dictionary)은 많은 값으 모음이다. 그러나 리스트의 인덱스와는 달리 사전의 인덱스는 정수만이 아닌 다양한 데이터 유형을 사용할 수 있다.<br>
+          사전을 위한 인덱스를 키(key)라고 하며, 키와 그에 연관된 값을 키-값 쌍(key-value pair)이라고 한다. 코드에서 사전은 중괄호 { }로 정의된다.</p>
+          <code>>>> myCat = {'size': 'fat', 'color': 'gray', 'disposition': 'loud'}</code>
+          <p>dnl zhemsms myCat 변수에 사전을 할당한다. 사전의 키는 'size', 'color', 'disposition' 이다.<br>
+            이들키에 대한 값은 각각 'fat', 'gray', 'loud'다. 이들 키를 통해 값을 사용할 수 있다.</p>
+        </article>
+      </section>
+
+    <h3>6장 문자열 조작하기</h3>
+
+
+    <p>&nbsp;</p>
+    <h2>2부 작업 자동화하기</h2>
+
+    <h3>7장 정규표현식을 사용한 패턴 대조</h3>
+    <h3>8장 파일 읽기 및 쓰기</h3>
+    <h3>9장 파일 조직화하기</h3>
+    <h3>10장 디버깅</h3>
+    <h3>11장 웹 스크랩</h3>
+    <h3>12장 엑셀 스프레드시트 다루기</h3>
+    <h3>13장 PDF 및 워드 문서 작업</h3>
+    <h3>14장 CSV 파일 및 JSON 데이터 작업</h3>
+    <h3>15장 시간 지키기, 작업 예약하기 및 프로그램 실행시키기</h3>
+    <h3>16장 전자메일 및 문자 메시지 전송</h3>
+    <h3>17장 이미지 조작</h3>
+    <h3>18장 키보드와 마우스 제어 및 GUI 자동화</h3>
+
+  </body>
+</html>
