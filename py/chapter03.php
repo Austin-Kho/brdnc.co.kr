ee831b62849b9dadb9b0bbb2bf42c6a6b53772c3
Austin-Kho <kori.susie@gmail.com>
2018-06-21 16:46:31 +0900

    챕터 3 시작

--- py/chapter03.php
+++ py/chapter03.php
@@ -2,7 +2,13 @@
     <div class="chapter">
       <section>
         <article class="">
+          <p>
+            <h5><strong>■ def 문과 매개변수</strong></h5>
+            <p>파이썬은 여러 가지 내장 함수를 제공하지만 필요한 함수를 직접 만들어 쓸 수도 있다.</p>
+            <p>함수를 부를 때에는 괄호 안에 입력하는 방식으로 값을 전달하며, 이것을 매개변수라고 부른다.</p>
+            <pre>>>> def hello(name):<br>       print('hello ' + name)<br>&nbsp;<br>>>> hello('Alice')<br>Hello Alice</pre>
 
+          </p>
         </article>
       </section>
     </div>
