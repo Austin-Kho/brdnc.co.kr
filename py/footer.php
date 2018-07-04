7e539c6fffb5d11e0b32177a5bbbcec8e2d8a5ce
austin <kori.susie@gmail.com>
2018-07-04 21:59:22 +0900

    스타일 개선

--- py/footer.php
+++ py/footer.php
@@ -1,5 +1,5 @@
     <div style="font-size: 25pt;">
-      <a id="MOVE_TOP_BTN" href="#" style=" color: #73a37b;"><span class="glyphicon glyphicon-circle-arrow-up" aria-hidden="true"></span></a>
+      <a id="MOVE_TOP_BTN" href="#" style=" color: #fb5f72;"><span class="glyphicon glyphicon-circle-arrow-up" aria-hidden="true"></span></a>
     </div>
     <br><br><br>
   </body>
