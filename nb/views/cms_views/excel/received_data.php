a6041c4a28c352c4dde9d4a659d0ab7c3b9166b6
Austin-Kho <kori.susie@gmail.com>
2019-08-22 15:58:44 +0900

    ì입출금 내역 엑셀 / 열조정

--- nb/views/cms_views/excel/received_data.php
+++ nb/views/cms_views/excel/received_data.php
@@ -1,8 +1,8 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-Header("Content-type: application/vnd.ms-excel; charset=UTF-8" );
-Header("Content-Disposition: attachment; filename=".iconv("UTF-8","cp949//IGNORE", $project->pj_name."_수납_데이터(".date('Y-m-d').").xls"));
-Header("Content-Description: PHP7 Generated Data" );
+defined('BASEPATH') or exit('No direct script access allowed');
+Header("Content-type: application/vnd.ms-excel; charset=UTF-8");
+Header("Content-Disposition: attachment; filename=" . iconv("UTF-8", "cp949//IGNORE", $project->pj_name . "_수납_데이터(" . date('Y-m-d') . ").xls"));
+Header("Content-Description: PHP7 Generated Data");
 Header("Pragma: no-cache");
 Header("Expires: 0");
 
@@ -10,20 +10,20 @@ Header("Expires: 0");
 echo $EXCEL_STR1 = "
   <table border='1'>
     <tr align='center' height='48'>
-      <td colspan='11' style='font-size:15pt; text-align:center;'><b>".$project->pj_name." 수납 데이터</b></td>
+      <td colspan='11' style='font-size:15pt; text-align:center;'><b>" . $project->pj_name . " 수납 데이터</b></td>
     </tr>
-    <tr><td  colspan='11' align='right' height='20' style='font-size:9pt;'>".date('Y-m-d')." 현재</td></tr>
+    <tr><td  colspan='11' align='right' height='20' style='font-size:9pt;'>" . date('Y-m-d') . " 현재</td></tr>
     <tr align='center' height='20' style='font-size:9pt;'>
       <td width='50' bgcolor='#EAEAEA'>no.</td>
-      <td width='90' bgcolor='#EAEAEA'>수납 일자</td>
-      <td width='100' bgcolor='#EAEAEA'>수납 금액</td>
-      <td width='120' bgcolor='#EAEAEA'>입금자</td>
-      <td width='100' bgcolor='#EAEAEA'>납입 회차</td>
-      <td width='100' bgcolor='#EAEAEA'>입금 계좌</td>
       <td width='80' bgcolor='#EAEAEA'>일련번호</td>
       <td width='90' bgcolor='#EAEAEA'>계약자</td>
       <td width='70' bgcolor='#EAEAEA'>타입</td>
       <td width='100' bgcolor='#EAEAEA'>동호수</td>
+      <td width='90' bgcolor='#EAEAEA'>수납 일자</td>
+      <td width='100' bgcolor='#EAEAEA'>수납 금액</td>
+      <td width='120' bgcolor='#EAEAEA'>입금자</td>
+      <td width='100' bgcolor='#EAEAEA'>입금 계좌</td>
+      <td width='100' bgcolor='#EAEAEA'>납입 회차</td>
       <td width='110' bgcolor='#EAEAEA'>당 건 총입금액</td>
     </tr>
   ";
@@ -50,22 +50,22 @@ foreach ($rec_data as $lt) :
       GROUP BY cb_cms_sales_received.cont_seq "
   );
 
-echo $EXCEL_STR2 = "
+  echo $EXCEL_STR2 = "
 <tr   height='20' style='font-size:9pt;'>
-	 <td align='center'>".$i."</td>
-	 <td align='center'>".$lt->paid_date."</td>
-	 <td align='right'>".number_format($lt->paid_amount)."</td>
-	 <td align='center'>".$lt->paid_who."</td>
-	 <td align='center'>".$lt->pay_name."</td>
-	 <td align='center'>".$lt->acc_nick."</td>
-   <td align='center'>".$code->cont_code."</td>
-	 <td align='center'>".$contractor->ct."</td>
-	 <td align='center'>".$lt->unit_type."</td>
-	 <td align='center'>".$lt->unit_dong_ho."</td>
-   <td align='right'>".number_format($total_rec->pa)."</td>
+   <td align='center'>" . $i . "</td>
+   <td align='center'>" . $code->cont_code . "</td>
+	 <td align='center'>" . $contractor->ct . "</td>
+	 <td align='center'>" . $lt->unit_type . "</td>
+	 <td align='center'>" . $lt->unit_dong_ho . "</td>
+	 <td align='center'>" . $lt->paid_date . "</td>
+	 <td align='right'>" . number_format($lt->paid_amount) . "</td>
+	 <td align='center'>" . $lt->paid_who . "</td>
+   <td align='center'>" . $lt->acc_nick . "</td>
+   <td align='center'>" . $lt->pay_name . "</td>
+   <td align='right'>" . number_format($total_rec->pa) . "</td>
 </tr>
    ";
-   $i++;
+  $i++;
 endforeach;
 
 echo $EXCEL_STR3 = "</table>";
